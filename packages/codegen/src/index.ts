a12d5039016b07983314240b8797b0052bcf5294
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-06-13 14:32:59 -0500

    cli

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -3,18 +3,16 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
+  .name('FHIR Code Generation')
+  .description('CLI to generate code based off fhir artifacts.')
   .version('0.8.0');
 
-program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
+program.command('generate-types')
+  .description('Generates typescript types off profiles')
+  .argument('<profiles...>', 'FHIR Version')
+  .option('-v, --version <version>', 'FHIR Profiles to use', "r4")
+  .action((profiles, options) => {
+    console.log(profiles, options)
   });
 
 program.parse();
