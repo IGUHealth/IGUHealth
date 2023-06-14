035b5a02066318b16a42947c31084b42f35937fe
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-06-14 14:18:25 -0500

    alt

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,5 +1,7 @@
 import { Command } from "commander";
 import { readFileSync, writeFileSync } from "fs";
+import resourceSDs from "@genfhi/artifacts/r4/profiles-resources.json";
+import typeSDs from "@genfhi/artifacts/r4/profiles-types.json";
 
 import { generateTypes, StructureDefinition } from "./typeGeneration";
 
@@ -15,15 +17,41 @@ program
   .version("0.8.0");
 
 program
-  .command("generate-types")
+  .command("generate-types-files")
   .description("Generates typescript types off profiles")
   .argument("<profiles...>", "FHIR profiles to use")
   .option("-o, --output <output>", "output file", "types.d.ts")
   .option("-v, --version <version>", "FHIR Profiles to use", "r4")
   .action((profiles, options) => {
+    if (options.version !== "r4") {
+      throw new Error("Currently only support r4");
+    }
     const bundles: Array<StructureDefinitionBundle> = profiles.map(
       (fileURI: string) => JSON.parse(readFileSync(fileURI, "utf8"))
     );
+
+    const structureDefinitions = bundles.flatMap((bundle) =>
+      bundle.entry.map((entry) => entry.resource)
+    );
+    const types = generateTypes(options.version, structureDefinitions);
+    writeFileSync(options.output, types);
+  });
+
+program
+  .command("generate-types-artifacts")
+  .description("Generates typescript types off profiles")
+  .option("-o, --output <output>", "output file")
+  .option("-v, --version <version>", "FHIR Profiles to use", "r4")
+  .action((options) => {
+    if (options.version !== "r4") {
+      throw new Error("Currently only support r4");
+    }
+    // Quick hack for generation
+    const bundles = [
+      resourceSDs as unknown as StructureDefinitionBundle,
+      typeSDs as unknown as StructureDefinitionBundle,
+    ];
+
     const structureDefinitions = bundles.flatMap((bundle) =>
       bundle.entry.map((entry) => entry.resource)
     );
