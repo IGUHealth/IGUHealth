cc607b34ce2cb3eb702c65d9e0c1259a3f200baa
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-20 17:36:43 -0400

    wip

---
+++ packages/components/src/basic/String.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import "./button.css";
+
+interface Props {
+  value: string;
+}
+
+const Main = styled.div`
+  color: red;
+`;
+
+export const String = ({ value = "", ...props }: Props) => {
+  return (
+    <Main>
+      foo
+      <input type="text" value={value} />
+    </Main>
+  );
+};
