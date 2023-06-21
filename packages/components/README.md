23ac9c7f4c23b4b6a721786bad84744ef4bac694
vorspiel-inator <137130778+vorspiel-inator@users.noreply.github.com>
2023-06-20 23:09:07 -0400

    Add components library (#14)
    Adds a structure for the components library.

---
+++ packages/components/README.md
@@ -0,0 +1,6 @@
+This directory was generated using the following commands:
+
+```
+pnpm create vite
+pnpm dlx storybook@latest init
+```
---
+++ packages/components/src/basic/String.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  value: string;
+}
+
+const Main = styled.div`
+  color: red;
+`;
+
+export const String = ({ value = "", ...props }: Props) => {
+  return (
+    <Main>
+      <input type="text" value={value} />
+    </Main>
+  );
+};
