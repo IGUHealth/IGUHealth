654610188582f18ac551a4ebba493d30152e1a11
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-06-22 16:56:22 -0500

    begin parsing

--- packages/fhir-server/src/index.ts
+++ packages/fhir-server/src/index.ts
@@ -1,6 +1,47 @@
 import Koa from "koa";
 
+import parseQuery, { FHIRURL } from "@genfhi/fhir-query";
+import { ResourceType, id } from "@genfhi/fhir-types/r4/types";
+
 const app = new Koa();
+
+type InteractionLevel = {
+  instance: "instance";
+  system: "system";
+  type: "type";
+};
+
+type InstanceLevelInteraction = {
+  level: InteractionLevel["instance"];
+  type: "read" | "vread" | "update" | "patch" | "delete" | "history";
+  resourceType: ResourceType;
+  id: id;
+};
+
+type TypeLevelInteractions = {
+  level: InteractionLevel["type"];
+  type: "create" | "search" | "delete" | "history";
+  resourceType: ResourceType;
+};
+
+type SystemInteraction = {
+  level: InteractionLevel["system"];
+  type: "capabilities" | "batch/transaction" | "delete" | "history" | "search";
+};
+
+function getLevel(fhirURL: FHIRURL): InteractionLevel[keyof InteractionLevel] {
+  if (fhirURL.resourceType && fhirURL.id) {
+    return "instance";
+  } else if (fhirURL.resourceType) {
+    return "type";
+  }
+  return "system";
+}
+
+function KoaRequestToFHIRRequest(request: Koa.Request) {
+  const method = request.method;
+  const fhirQuery = parseQuery(request.URL);
+}
 
 app.use(async (ctx, next) => {
   await next();
@@ -11,7 +52,7 @@ app.use(async (ctx, next) => {
 // x-response-time
 
 app.use(async (ctx, next) => {
-  console.log(ctx.URL);
+  // console.log(ctx.URL);
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
@@ -23,4 +64,5 @@ app.use(async (ctx) => {
   ctx.body = "Hello Worlds";
 });
 
+console.log("Running app");
 app.listen(3000);
