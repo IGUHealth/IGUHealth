107eff1fa200d49ade0fc4a7866bd72e5a084ca7
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-23 16:28:46 -0400

    PR feedback, types

--- packages/components/src/basic/DateTime/DateTime.tsx
+++ packages/components/src/basic/DateTime/DateTime.tsx
@@ -1,20 +1,17 @@
-import React from "react";
+import React, { FC } from "react";
 import dayjs from "dayjs";
 import locale from "antd/locale/en_US";
 import { DatePicker, ConfigProvider } from "antd";
 import type { dateTime } from "@genfhi/fhir-types/r4/types";
 
-interface Props {
-  value: dateTime;
-  onChange: (newValue: dateTime | null) => void;
-}
+type Props = FhirWidgetProps<dateTime>
 
 const validRegex =
   /^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]{1,9})?)?)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)?)?)?$/;
 
 const Invalid = () => <div>(invalid)</div>;
 
-export const Datetime = ({ value = "", onChange, ...props }: Props) => {
+export const Datetime: FC<Props> = ({ value = "", onChange }) => {
   if (!validRegex.test(value)) {
     // TODO
     return <Invalid />;
