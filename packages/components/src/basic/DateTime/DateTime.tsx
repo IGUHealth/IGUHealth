cdb5a64e2b2e96a874b8428fa30898477bbc41c7
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-23 16:23:50 -0400

    Remove styled-components

--- packages/components/src/basic/DateTime/DateTime.tsx
+++ packages/components/src/basic/DateTime/DateTime.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import dayjs from 'dayjs';
 import locale from 'antd/locale/en_US';
 import { DatePicker, ConfigProvider } from "antd"
@@ -10,16 +9,10 @@ interface Props {
   onChange: (newValue: dateTime | null) => void
 }
 
-const Main = styled.div`
-  color: red;
-`;
-
 const validRegex =
   /^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]{1,9})?)?)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)?)?)?$/;
 
-
 const Invalid = () => <div>(invalid)</div>
-
 
 export const Datetime = ({ value = "",  onChange, ...props }: Props) => {
   if (!validRegex.test(value)) {
@@ -47,7 +40,6 @@ export const Datetime = ({ value = "",  onChange, ...props }: Props) => {
   }
 
   return (
-    <Main>
       <ConfigProvider locale={locale}>
         <DatePicker
           defaultValue={parsed}
@@ -62,6 +54,5 @@ export const Datetime = ({ value = "",  onChange, ...props }: Props) => {
           }}
         />
       </ConfigProvider>
-    </Main>
   );
 };
