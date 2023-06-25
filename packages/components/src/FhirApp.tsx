9e60476977fa2bce1053d5f1041135bae370bae1
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-25 00:02:48 -0400

    Add tailwind

--- packages/components/src/FhirApp.tsx
+++ packages/components/src/FhirApp.tsx
@@ -2,12 +2,12 @@ import React, { FC, ReactNode } from "react";
 import locale from "antd/locale/en_US";
 import { ConfigProvider } from "antd";
 
-import styles from './index.css?inline'
+import tailwindStyles from '../dist/output.css?inline'
 
 export const FhirApp: FC<{children: ReactNode}> = ({ children }) => {
   return (
     <ConfigProvider locale={locale}>
-      <style>{styles}</style>
+      <style>{tailwindStyles}</style>
       {children}
     </ConfigProvider>
   );
