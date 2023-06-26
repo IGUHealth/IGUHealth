cac86d505ab9fa958168f2a137d026c1407b2070
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-26 18:42:14 -0400

    add back

--- packages/components/src/main.tsx
+++ packages/components/src/main.tsx
@@ -1,11 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { FhirApp } from './FhirApp'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <FhirApp>
-      Hello world
-    </FhirApp>
-  </React.StrictMode>,
-)
+export { FhirApp } from './FhirApp'
