f5f16cdece7376dda988406c0c15ee9c8968e3d1
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-26 18:35:27 -0400

    wip

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: 'GenFHI-ts',
+      fileName: 'genfhi-ts',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+
 })
