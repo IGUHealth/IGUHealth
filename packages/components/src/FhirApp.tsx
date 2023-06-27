b80b4593cb11181f32a8255fd883e1e23d519e73
Vorspiel-inator <vorspiel-inator@users.noreply.gitlab.com>
2023-06-27 12:59:12 -0400

    Try to import tailwind css

--- packages/components/src/FhirApp.tsx
+++ packages/components/src/FhirApp.tsx
@@ -2,12 +2,9 @@ import React, { FC, ReactNode } from "react";
 import locale from "antd/locale/en_US";
 import { ConfigProvider } from "antd";
 
-import tailwindStyles from '../dist/output.css?inline'
-
 export const FhirApp: FC<{children: ReactNode}> = ({ children }) => {
   return (
     <ConfigProvider locale={locale}>
-      <style>{tailwindStyles}</style>
       <div className="Fhir__FhirApp">
       {children}
       </div>
