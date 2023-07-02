4e4b84342bf656dcc2d50a3c7ffbf632428e8885
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-07-02 12:51:38 -0500

    alts

--- packages/fhir-server/src/client/middleware/index.test.ts
+++ packages/fhir-server/src/client/middleware/index.test.ts
@@ -1,10 +1,8 @@
 import { FHIRURL } from "@genfhi/fhir-query";
-import { Resource } from "@genfhi/fhir-types/r4/types";
-import { FHIRRequest } from "../types";
-import createMiddleware, { MiddlewareSync } from "./index";
+import { createMiddlewareSync, createMiddlewareAsync } from "./index";
 
-test("Test middleware", () => {
-  const middleware = createMiddleware<{}, {}>([
+test("Test middleware Sync", () => {
+  const middleware = createMiddlewareSync<{}, {}>([
     (request, args, next) => {
       if (next) {
         const nextVal = next(request, args);
@@ -31,7 +29,7 @@ test("Test middleware", () => {
       };
     },
     (request, args, next) => {
-      const body: Resource[] = [{ resourceType: "Patient" }];
+      const body = [{ resourceType: "Patient" }];
       return {
         state: args.state,
         ctx: args.ctx,
@@ -51,7 +49,62 @@ test("Test middleware", () => {
       query: undefined,
       type: "search-response",
       level: "system",
-      body: [{ resourceType: "Patient" }],
+      body: [{ id: "123", resourceType: "Patient" }],
+    },
+  });
+});
+
+test("Test middleware Async", async () => {
+  const middleware = createMiddlewareAsync<{}, {}>([
+    async (request, args, next) => {
+      if (next) {
+        const nextVal = await next(request, args);
+        return {
+          ...nextVal,
+          response: {
+            ...nextVal.response,
+            body: (nextVal.response as any).body?.map((resource) => ({
+              ...resource,
+              id: "123",
+            })),
+          },
+        };
+      }
+      return {
+        state: args.state,
+        ctx: args.ctx,
+        response: {
+          query: (request as any).query as FHIRURL,
+          type: "search-response",
+          level: "system",
+          body: [],
+        },
+      };
+    },
+    async (request, args, next) => {
+      const body = [{ resourceType: "Patient" }];
+      return {
+        state: args.state,
+        ctx: args.ctx,
+        response: {
+          query: (request as any).query as FHIRURL,
+          type: "search-response",
+          level: "system",
+          body: body,
+        },
+      };
+    },
+  ]);
+  const result = middleware({} as any, { state: {}, ctx: {} });
+  expect(result).toBeInstanceOf(Promise);
+  expect(await result).toEqual({
+    state: {},
+    ctx: {},
+    response: {
+      query: undefined,
+      type: "search-response",
+      level: "system",
+      body: [{ id: "123", resourceType: "Patient" }],
     },
   });
 });
--- packages/fhir-server/src/client/middleware/index.ts
+++ packages/fhir-server/src/client/middleware/index.ts
@@ -12,14 +12,29 @@ export type MiddlewareSync<State, CTX> = (
   next?: MiddlewareSync<State, CTX>
 ) => { ctx: CTX; state: State; response: FHIRResponse };
 
-export default function createMiddleware<State, CTX>(
+export function createMiddlewareSync<State, CTX>(
   middleware: MiddlewareSync<State, CTX>[]
 ): MiddlewareSync<State, CTX> {
   return (request, args) => {
     const [first, ...rest] = middleware;
     if (rest[0]) {
       return first(request, args, (req, arg) => {
-        return createMiddleware(rest)(req, arg);
+        return createMiddlewareSync(rest)(req, arg);
+      });
+    } else {
+      return first(request, args);
+    }
+  };
+}
+
+export function createMiddlewareAsync<State, CTX>(
+  middleware: MiddlewareAsync<State, CTX>[]
+): MiddlewareAsync<State, CTX> {
+  return (request, args) => {
+    const [first, ...rest] = middleware;
+    if (rest[0]) {
+      return first(request, args, (req, arg) => {
+        return createMiddlewareAsync(rest)(req, arg);
       });
     } else {
       return first(request, args);
