30e009e4541e248ab8e111136d9c6473eef2851a
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-07-02 19:53:50 -0500

    alt

---
+++ packages/fhir-server/src/resourceProviders/postgres/migrations/sqls/20230703004327-string-table-up.sql
@@ -0,0 +1,16 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE string_search (
+  workspace     UUID        NOT NULL, 
+  r_version_id  SERIAL      NOT NULL PRIMARY KEY,
+  name          TEXT        NOT NULL,
+  parameter_url TEXT        NOT NULL,
+  value         TEXT        NOT NULL,
+  
+  -- When was indexed
+  created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_resource
+      FOREIGN KEY(r_version_id) 
+	  REFERENCES resources(version_id)
+);
