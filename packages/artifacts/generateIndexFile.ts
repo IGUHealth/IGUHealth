6b4ad5feef40448aa166e180b2d461b6bba6e5e4
gen-fhi <93403502+gen-fhi@users.noreply.github.com>
2023-07-04 13:54:45 -0500

    Setting up testing fp (#44)
    * init
    
    * qfix
    
    * qfix
    
    ---------
    
    Co-authored-by: S O <9434934-karate-kid--@users.noreply.gitlab.com>

---
+++ packages/artifacts/generateIndexFile.ts
@@ -0,0 +1,64 @@
+import path from "path";
+import fs from "node:fs";
+
+import { Bundle } from "@genfhi/fhir-types/r4/types";
+
+import { IndexFile } from "./types";
+
+function getAllFiles(directory: string): string[] {
+  const files: string[] = [];
+  const filesInDirectory = fs.readdirSync(directory);
+  for (const file of filesInDirectory) {
+    const absolute = path.join(directory, file);
+    if (fs.statSync(absolute).isDirectory()) {
+      files.push(...getAllFiles(absolute));
+    } else {
+      files.push(absolute);
+    }
+  }
+  return files;
+}
+
+function checkBundleResourceTypesAlign(b: Bundle): boolean {
+  const resourceTypes = (b.entry || []).map((e) => e.resource?.resourceType);
+  const set = new Set(resourceTypes);
+  return set.size === 1 && resourceTypes[0] !== undefined;
+}
+
+function generateIndexFile(root: string, artifactLocations: string[]) {
+  // Read artifactLocation and recursively walk the directory tree reading all files from root
+  // For each file, read the contents and parse the JSON
+  // If the JSON has a "resourceType" property, add it to the index
+
+  const index: IndexFile = { "index-version": "1", files: [] };
+  const files = artifactLocations
+    .map((loc) => path.join(root, loc))
+    .map(getAllFiles)
+    .flat()
+    .filter((f) => f.endsWith(".json"));
+
+  for (let file of files) {
+    const fileContents = fs.readFileSync(file);
+    const json = JSON.parse(fileContents.toString("utf8"));
+    if (json.resourceType === "Bundle") {
+      const bundle = json as Bundle;
+      // Add to index
+      checkBundleResourceTypesAlign(bundle);
+      index.files = (index.files || []).concat({
+        filename: file,
+        resourceType: bundle.entry?.[0].resource?.resourceType,
+      });
+    } else if (json.resourceType) {
+      // Add to index
+      index.files = (index.files || []).concat([
+        {
+          filename: file,
+          resourceType: json.resourceType,
+        },
+      ]);
+    }
+  }
+  console.log(index);
+}
+
+generateIndexFile("./", ["./r4"]);
