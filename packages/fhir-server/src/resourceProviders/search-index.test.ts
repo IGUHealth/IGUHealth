7d5fc90a7ae887232d3b51077912a3ec515fcff6
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-07-06 13:15:27 -0500

    beginning dynamic testing parameters

--- packages/fhir-server/src/resourceProviders/search-index.test.ts
+++ packages/fhir-server/src/resourceProviders/search-index.test.ts
@@ -4,6 +4,7 @@ import { ResourceType, Resource } from "@genfhi/fhir-types/r4/types";
 import { resourceTypes } from "@genfhi/fhir-types/r4/sets";
 import { expect, test } from "@jest/globals";
 import { loadArtifacts } from "@genfhi/artifacts";
+import * as fhirpath from "@genfhi/fhirpath";
 
 import MemoryDatabase from "./memory";
 import { FHIRClientSync } from "../client/interface";
@@ -30,11 +31,32 @@ const memDatabase = createMemoryDatabase([
 test.each([...resourceTypes.values()])(
   `Testing indexing resourceType '%s'`,
   (resourceType) => {
+    const searchParameters = memDatabase.search_type({}, "SearchParameter", {
+      resourceType: "SearchParameter",
+      parameters: {
+        type: {
+          name: "type",
+          value: ["string"],
+        },
+        base: {
+          name: "base",
+          value: [resourceType],
+        },
+      },
+    });
     const resources = memDatabase.search_type(
       {},
       resourceType as ResourceType,
       { parameters: {} }
     );
-    console.log(`${resourceType}`, resources.length);
+    for (const resource of resources.slice(0, 5)) {
+      for (const parameter of searchParameters) {
+        if (parameter.expression) {
+          expect(
+            fhirpath.evaluate(parameter.expression, resource)
+          ).toMatchSnapshot();
+        }
+      }
+    }
   }
 );
