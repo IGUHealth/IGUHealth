3bc50b072a031f417a0053a0bc687237dc8c28c4
gen-fhi <93403502+gen-fhi@users.noreply.github.com>
2023-07-08 14:11:13 -0500

    Finish indices (#56)
    * init
    
    * numeric indice
    
    * alterations
    
    * quantity
    
    ---------
    
    Co-authored-by: S O <9434934-karate-kid--@users.noreply.gitlab.com>

---
+++ packages/fhir-server/src/resourceProviders/postgres/migrations/sqls/20230708165910-remaining-indices-up.sql
@@ -0,0 +1,105 @@
+CREATE TABLE date_idx (
+  id             SERIAL      NOT NULL PRIMARY KEY,
+  workspace      UUID        NOT NULL, 
+  r_id           TEXT        NOT NULL,
+  r_version_id   SERIAL      NOT NULL,
+  parameter_name TEXT        NOT NULL,
+  parameter_url  TEXT        NOT NULL,
+
+  start_date     TIMESTAMPTZ NOT NULL,
+  end_date       TIMESTAMPTZ NOT NULL,
+  
+  -- When was indexed
+  created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_resource
+      FOREIGN KEY(r_version_id) 
+	  REFERENCES resources(version_id)
+);
+
+CREATE TABLE token_idx (
+  id             SERIAL      NOT NULL PRIMARY KEY,
+  workspace      UUID        NOT NULL, 
+  r_id           TEXT        NOT NULL,
+  r_version_id   SERIAL      NOT NULL,
+  parameter_name TEXT        NOT NULL,
+  parameter_url  TEXT        NOT NULL,
+
+  uri            TEXT        NOT NULL,
+  value          TEXT        NOT NULL,
+  
+  -- When was indexed
+  created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_resource
+      FOREIGN KEY(r_version_id) 
+	  REFERENCES resources(version_id)
+);
+
+CREATE TABLE reference_idx (
+  id             SERIAL      NOT NULL PRIMARY KEY,
+  workspace      UUID        NOT NULL, 
+  r_id           TEXT        NOT NULL,
+  r_version_id   SERIAL      NOT NULL,
+  parameter_name TEXT        NOT NULL,
+  parameter_url  TEXT        NOT NULL,
+
+  reference      JSONB       NOT NULL,
+  -- Should be generating off of these from reference type with procedure?
+  resource_type  TEXT        NOT NULL,
+  resource_id    TEXT        NOT NULL,
+  
+  -- When was indexed
+  created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_resource
+      FOREIGN KEY(r_version_id) 
+	  REFERENCES resources(version_id)
+);
+
+
+CREATE TABLE quantity_idx (
+  id             SERIAL      NOT NULL PRIMARY KEY,
+  workspace      UUID        NOT NULL, 
+  r_id           TEXT        NOT NULL,
+  r_version_id   SERIAL      NOT NULL,
+  parameter_name TEXT        NOT NULL,
+  parameter_url  TEXT        NOT NULL,
+
+  start_quantity JSONB       NOT NULL,
+   -- All these Values can be null given quantity type
+  start_value          NUMERIC     GENERATED ALWAYS AS (CAST(start_quantity ->> 'value'  as NUMERIC)) STORED,
+  start_system         TEXT        GENERATED ALWAYS AS (CAST(start_quantity ->> 'system' as TEXT))    STORED,
+  start_code           TEXT        GENERATED ALWAYS AS (CAST(start_quantity ->> 'code'   as TEXT))    STORED,
+
+  end_quantity JSONB         NOT NULL,
+   -- All these Values can be null given quantity type
+  end_value          NUMERIC     GENERATED ALWAYS AS (CAST(end_quantity ->> 'value'  as NUMERIC)) STORED,
+  end_system         TEXT        GENERATED ALWAYS AS (CAST(end_quantity ->> 'system' as TEXT))    STORED,
+  end_code           TEXT        GENERATED ALWAYS AS (CAST(end_quantity ->> 'code'   as TEXT))    STORED,
+  
+  -- When was indexed
+  created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_resource
+      FOREIGN KEY(r_version_id) 
+	  REFERENCES resources(version_id)
+);
+
+CREATE TABLE uri_idx (
+  id             SERIAL      NOT NULL PRIMARY KEY,
+  workspace      UUID        NOT NULL, 
+  r_id           TEXT        NOT NULL,
+  r_version_id   SERIAL      NOT NULL,
+  parameter_name TEXT        NOT NULL,
+  parameter_url  TEXT        NOT NULL,
+
+  value          TEXT        NOT NULL,
+  
+  -- When was indexed
+  created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+  CONSTRAINT fk_resource
+      FOREIGN KEY(r_version_id) 
+	  REFERENCES resources(version_id)
+);
