8f3f2813712ca88cdba1cc7451542edd8f11fa47
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-07-21 17:28:10 -0500

    validate plug

--- packages/fhir-server/src/fhirServer.ts
+++ packages/fhir-server/src/fhirServer.ts
@@ -6,7 +6,11 @@ import {
   StructureDefinition,
   Resource,
 } from "@iguhealth/fhir-types/r4/types";
-import { OperationError, outcomeError } from "@iguhealth/operation-outcomes";
+import {
+  OperationError,
+  outcome,
+  outcomeError,
+} from "@iguhealth/operation-outcomes";
 
 import { createMiddlewareAsync } from "./client/middleware/index.js";
 import { FHIRClient } from "./client/interface.js";
@@ -18,6 +22,7 @@ import {
   InstanceLevelInteraction,
   SystemInteraction,
 } from "./client/types";
+import validate from "@iguhealth/fhir-validation";
 
 function getInteractionLevel(
   fhirURL: FHIRURL
@@ -171,6 +176,45 @@ export type FHIRServerCTX = {
 
 function createFHIRServer() {
   return createMiddlewareAsync<undefined, FHIRServerCTX>([
+    async (request, { state, ctx }, next) => {
+      switch (request.type) {
+        case "update-request":
+        case "create-request":
+        case "batch-request":
+        case "transaction-request": {
+          const issues = validate(
+            (type) => {
+              const sd = ctx.resolveSD(ctx, type);
+              if (!sd)
+                throw new OperationError(
+                  outcomeError(
+                    "invalid",
+                    `Could not validate type of ${request.body.resourceType}`
+                  )
+                );
+              return sd;
+            },
+            request.body.resourceType,
+            request.body
+          );
+          if (issues.length > 0) {
+            throw new OperationError(outcome(issues));
+          }
+          break;
+        }
+        case "patch-request": {
+          throw new OperationError(
+            outcomeError(
+              "not-supported",
+              `Operation '${request.type}' not supported`
+            )
+          );
+        }
+        default:
+      }
+      if (!next) throw new Error("No next");
+      return next(request, { state, ctx });
+    },
     async (request, { state, ctx }, next) => {
       return {
         state,
