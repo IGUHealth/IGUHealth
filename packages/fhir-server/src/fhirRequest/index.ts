d926b660f5695ddec9b13917cddad63e957b496e
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-07-26 15:55:41 -0500

    cleanup

--- packages/fhir-server/src/fhirRequest/index.ts
+++ packages/fhir-server/src/fhirRequest/index.ts
@@ -105,31 +105,48 @@ function parseSystemRequest(
  ** https://build.fhir.org/http.html#summary
  **
  -----------------------------------------------------------------------------------------------------------------
-read            	  /[type]/[id]	                      GET‡	N/A	N/A	N/A	O: If-Modified-Since, If-None-Match
-vread            	  /[type]/[id]/_history/[vid]	        GET‡	N/A	N/A	N/A	N/A
-update             	/[type]/[id]                      	PUT	R	Resource	O	O: If-Match
-patch        	      /[type]/[id]                      	PATCH	R (may be a patch type)	Patch	O	O: If-Match
-delete	            /[type]/[id]	                      DELETE	N/A	N/A	N/A	N/A
-create         	    /[type]                           	POST	R	Resource	O	O: If-None-Exist
-search-type	        /[type]?                           	GET	N/A	N/A	N/A	N/A
-                    /[type]/_search?	                  POST	application/x-www-form-urlencoded	form data	N/A	N/A
-search-system	?	                                        GET	N/A	N/A	N/A	N/A
-/_search	                                              POST	application/x-www-form-urlencoded	form data	N/A	N/A
 search-compartment	/[compartment]/[id]/*?	            GET	N/A	N/A	N/A	N/A
                     /[compartment]/[id]/[type]?	        GET	N/A	N/A	N/A	N/A
                     /[compartment]/[id]/_search?	      POST	application/x-www-form-urlencoded	form data	N/A	N/A
                     /[compartment]/[id]/[type]/_search?	POST	application/x-www-form-urlencoded	form data	N/A	N/A
-capabilities	      /metadata	                          GET‡	N/A	N/A	N/A	N/A
+
 transaction	        /	                                  POST	R	Bundle	O	N/A
 batch	              /	                                  POST	R	Bundle	O	N/A
-history-instance	  /[type]/[id]/_history	              GET	N/A	N/A	N/A	N/A
-history-type	      /[type]/_history	                  GET	N/A	N/A	N/A	N/A
+search-system	      ?	                                  GET	N/A	N/A	N/A	N/A
+
+capabilities	      /metadata	                          GET‡	N/A	N/A	N/A	N/A
+create         	    /[type]                           	POST	R	Resource	O	O: If-None-Exist
+search-type	        /[type]?                           	GET	N/A	N/A	N/A	N/A
+search-system       /_search	                          POST	application/x-www-form-urlencoded	form data	N/A	N/A
 history-system	    /_history	                          GET	N/A	N/A	N/A	N/A
-(operation)	        /$[name]
-                    /[type]/$[name]
-                    /[type]/[id]/$[name]	              POST	R	Parameters	N/A	N/A 
+(operation)	        /$[name]                            POST	R	Parameters	N/A	N/A 
                                                         GET	N/A	N/A	N/A	N/A
                                                         POST	application/x-www-form-urlencoded	form data	N/A	N/A
+
+(operation)	        /[type]/$[name]                     POST	R	Parameters	N/A	N/A 
+                                                        GET	N/A	N/A	N/A	N/A
+                                                        POST	application/x-www-form-urlencoded	form data	N/A	N/A
+search-type         /[type]/_search?	                  POST	application/x-www-form-urlencoded	form data	N/A	N/A
+read            	  /[type]/[id]	                      GET‡	N/A	N/A	N/A	O: If-Modified-Since, If-None-Match
+update             	/[type]/[id]                      	PUT	R	Resource	O	O: If-Match
+patch        	      /[type]/[id]                      	PATCH	R (may be a patch type)	Patch	O	O: If-Match
+delete	            /[type]/[id]	                      DELETE	N/A	N/A	N/A	N/A
+history-type	      /[type]/_history	                  GET	N/A	N/A	N/A	N/A
+
+(operation)         /[type]/[id]/$[name]                POST	R	Parameters	N/A	N/A 
+                                                        GET	N/A	N/A	N/A	N/A
+                                                        POST	application/x-www-form-urlencoded	form data	N/A	N/A
+history-instance	  /[type]/[id]/_history	              GET	N/A	N/A	N/A	N/A
+
+vread            	  /[type]/[id]/_history/[vid]	        GET‡	N/A	N/A	N/A	N/A
+
+
+
+
+
+
+
+
  -----------------------------------------------------------------------------------------------------------------
 */
 
@@ -139,6 +156,18 @@ export function KoaRequestToFHIRRequest(
 ): FHIRRequest {
   const fhirQuery = parseQuery(url);
   const level = getInteractionLevel(fhirQuery);
+
+  const method = request.method;
+  const urlPieces = url.split("/");
+  if (urlPieces.length === 1) {
+    return {
+      type: "capabilities-request",
+      level: "system",
+    };
+  } else if (urlPieces.length === 2) {
+  } else if (urlPieces.length === 3) {
+  } else if (urlPieces.length === 4) {
+  }
 
   switch (level) {
     case "instance":
