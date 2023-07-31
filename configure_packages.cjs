9e1979b6f7e5a56cf9700f569a656b2e1d218a51
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-07-31 12:05:22 -0500

    alt

--- configure_packages.cjs
+++ configure_packages.cjs
@@ -4,11 +4,12 @@ const fs = require("node:fs");
 fs.readdirSync(path.join(__dirname, "packages"))
   .map((file) => path.join(__dirname, "packages", file, "package.json"))
   .filter((filename) => fs.existsSync(filename))
-  .forEach((packageJSONFile) => {
+  .forEach((packageJSONFILE) => {
     console.log("Configuring package.json", packageJSONFILE);
     let packageJSON = require(packageJSONFILE);
     packageJSON = {
-      version: "0.0.4",
+      ...packageJSON,
+      version: packageJSON.version ? packageJSON.version : "0.0.1",
       scripts: {
         ...packageJSON.scripts,
         patch: "yarn version patch",
