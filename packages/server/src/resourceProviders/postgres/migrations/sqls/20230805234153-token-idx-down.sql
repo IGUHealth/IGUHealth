cb256d51adeae4ac9b2b4e616882987227ea0018
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-08-05 18:48:45 -0500

    quick indices for speedup

--- packages/server/src/resourceProviders/postgres/migrations/sqls/20230805234153-token-idx-down.sql
+++ packages/server/src/resourceProviders/postgres/migrations/sqls/20230805234153-token-idx-down.sql
@@ -1,1 +1,2 @@
 DROP INDEX idx_token_idx;
+DROP INDEX idx_string_idx;
--- packages/server/src/resourceProviders/postgres/migrations/sqls/20230805234153-token-idx-up.sql
+++ packages/server/src/resourceProviders/postgres/migrations/sqls/20230805234153-token-idx-up.sql
@@ -1,1 +1,2 @@
 CREATE INDEX idx_token_idx on token_idx(workspace, parameter_url, value);
+CREATE INDEX idx_string_idx on token_idx(workspace, parameter_url, value);
