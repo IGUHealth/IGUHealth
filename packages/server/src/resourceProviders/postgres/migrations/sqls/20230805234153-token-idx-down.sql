dc981e50f7e5f0e10289cabc972588466a7bb358
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-08-05 19:25:27 -0500

    qindices

--- packages/server/src/resourceProviders/postgres/migrations/sqls/20230805234153-token-idx-down.sql
+++ packages/server/src/resourceProviders/postgres/migrations/sqls/20230805234153-token-idx-down.sql
@@ -1,3 +1,18 @@
+DROP INDEX idx_number_idx_rid;
+DROP INDEX idx_number_idx_value;
+
+DROP INDEX idx_uri_idx_rid;
+DROP INDEX idx_uri_idx_value;
+
+DROP INDEX idx_quantity_idx_r_id;
+-- DROP INDEX idx_quantity_idx_value;
+
+DROP INDEX idx_reference_idx_rid;
+DROP INDEX idx_reference_idx_value;
+
+DROP INDEX idx_date_idx_r_id;
+-- DROP INDEX idx_date_idx_value;
+
 DROP INDEX idx_token_idx_value;
 DROP INDEX idx_token_idx_rid;
 
