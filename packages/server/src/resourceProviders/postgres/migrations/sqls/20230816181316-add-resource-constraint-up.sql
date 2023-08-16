2ec853650e23d8bba8138ad7047d3490cdc9d106
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-08-16 16:24:38 -0500

    alterations

--- packages/server/src/resourceProviders/postgres/migrations/sqls/20230816181316-add-resource-constraint-up.sql
+++ packages/server/src/resourceProviders/postgres/migrations/sqls/20230816181316-add-resource-constraint-up.sql
@@ -1,1 +1,23 @@
 ALTER TABLE resources ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+
+ALTER TABLE date_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE date_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+ALTER TABLE number_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE number_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+ALTER TABLE quantity_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE quantity_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+ALTER TABLE reference_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE reference_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+ALTER TABLE string_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE string_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+ALTER TABLE token_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE token_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
+
+ALTER TABLE uri_idx ALTER COLUMN workspace SET DATA TYPE TEXT;
+ALTER TABLE uri_idx ADD CONSTRAINT workspace_constraint FOREIGN KEY (workspace)  REFERENCES workspaces(id);
