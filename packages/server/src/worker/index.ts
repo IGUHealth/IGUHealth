3f65424f11cb071d95b244e3224b7232d60620e1
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-08-17 12:39:38 -0500

    sub latest

--- packages/server/src/worker/index.ts
+++ packages/server/src/worker/index.ts
@@ -5,6 +5,11 @@ import pg from "pg";
 import createServiceCTX from "../ctx/index.js";
 import logAuditEvent, { SERIOUS_FAILURE } from "../logging/auditEvents.js";
 import { KoaRequestToFHIRRequest } from "../fhirRequest/index.js";
+import { OperationError, outcomeError } from "@iguhealth/operation-outcomes";
+import {
+  SystemSearchResponse,
+  TypeSearchResponse,
+} from "@iguhealth/client/lib/types.js";
 
 dotEnv.config();
 
@@ -26,9 +31,10 @@ async function subWorker(loopInterval = 100) {
     ).rows.map((row) => row.id);
 
     for (const workspace of activeWorkspaces) {
+      const ctx = { ...services, workspace, author: "system" };
       console.log(`Processing '${workspace}' subscriptions`);
       const activeSubscriptions = await services.client.search_type(
-        { ...services, workspace, author: "system" },
+        ctx,
         "Subscription",
         [{ name: "status", value: ["active"] }]
       );
@@ -38,22 +44,52 @@ async function subWorker(loopInterval = 100) {
           const request = KoaRequestToFHIRRequest(subscription.criteria, {
             method: "GET",
           });
-          const result = await services.client.request(
-            { ...services, workspace, author: "system" },
-            request
+          if (request.type !== "search-request") {
+            throw new OperationError(
+              outcomeError(
+                "invalid",
+                `Criteria must be a search request but found ${request.type}`
+              )
+            );
+          }
+          const sortParameter = request.parameters.find(
+            (p) => p.name === "_sort"
           );
-          console.log(result);
+
+          if (sortParameter) {
+            throw new OperationError(
+              outcomeError(
+                "invalid",
+                `Criteria cannot include _sort. Sorting must be based order resource was updated.`
+              )
+            );
+          }
+
+          const getLatestVersionIdForSub = await services.cache.get(
+            ctx,
+            `${subscription.id}_latest`
+          );
+
+          if (getLatestVersionIdForSub) {
+            const result = (await services.client.request(ctx, request)) as
+              | TypeSearchResponse
+              | SystemSearchResponse;
+
+            for (const resource of result.body) {
+            }
+          }
         } catch (e) {
+          console.error(e);
           await logAuditEvent(
-            { ...services, workspace, author: "system" },
+            ctx,
             SERIOUS_FAILURE,
             { reference: `Subscription/${subscription.id}` },
             "error processing subscription"
           );
-          await services.client.update(
-            { ...services, workspace, author: "system" },
-            { ...subscription, status: "error" }
-          );
+          await services.client.update(ctx, {
+            ...subscription,
+            status: "error",
+          });
         }
       }
     }
