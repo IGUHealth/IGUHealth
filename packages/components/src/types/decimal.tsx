74f881214255b415e75d85fc649bf19443f2e937
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-08-31 13:19:07 -0500

    alterations

---
+++ packages/components/src/types/decimal.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { LabelContainer } from "../base/labelContainer";
+
+export interface DecimalProps {
+  value?: number;
+  onChange: (value: number) => void;
+  issue?: string;
+  label: string;
+}
+
+export const Decimal = ({ value, onChange, issue, label }: DecimalProps) => {
+  return (
+    <LabelContainer label={label} issues={issue ? [issue] : []}>
+      <input
+        type="number"
+        value={value}
+        step="0.1"
+        onChange={(e) => {
+          const value = parseFloat(e.target.value);
+          if (onChange && !isNaN(value)) {
+            onChange(value);
+          }
+        }}
+      />
+    </LabelContainer>
+  );
+};
