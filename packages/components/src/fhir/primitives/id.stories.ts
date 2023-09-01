cc7c086d250d7c0079767ff5e1ac87eca11faacb
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-09-01 16:29:16 -0500

    id

---
+++ packages/components/src/fhir/primitives/id.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Id } from "./id";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Primitives/id",
+  component: Id,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof Id>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    value: "test",
+    onChange: (value: string) => console.log(value),
+  },
+};
+
+export const InvalidId: Story = {
+  args: {
+    value: "*asdf",
+    onChange: (value: string) => console.log(value),
+  },
+};
+
+export const Error: Story = {
+  args: {
+    value: "test",
+    issue: "Bad value",
+    onChange: (value: string) => console.log(value),
+  },
+};
---
+++ packages/components/src/fhir/primitives/id.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { LabelContainer } from "../../base/labelContainer";
+
+export interface IdProps {
+  /**
+   * The value of the input.
+   */
+  value: string;
+  /**
+   * The value of the input.
+   */
+  issue?: string;
+  /**
+   * Call back triggered when input changes.
+   */
+  onChange?: (value: string) => void;
+  /**
+   * Label string.
+   */
+  label?: string;
+}
+
+const idRegex = /^[A-Za-z0-9\-\.]{1,64}$/;
+
+export const Id = ({ onChange, value, issue, label }: IdProps) => {
+  const [issues, setIssues] = React.useState<string[]>([]);
+  React.useEffect(() => {
+    const issues: string[] = [];
+    if (value && !idRegex.test(value)) {
+      issues.push(`Id is in an invalid format`);
+    }
+    if (issue) issues.push(issue);
+    setIssues(issues);
+  }, [value, issue]);
+
+  return (
+    <LabelContainer label={label} issues={issues}>
+      <input
+        className="outline-none"
+        type="text"
+        value={value}
+        onChange={(e) => {
+          if (onChange) {
+            onChange(e.target.value);
+          }
+        }}
+      />
+    </LabelContainer>
+  );
+};
