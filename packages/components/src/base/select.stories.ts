544bf398651368426743b70064555e9e1f3af286
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-09-01 10:59:28 -0500

    select open and nullable

--- packages/components/src/base/select.stories.ts
+++ packages/components/src/base/select.stories.ts
@@ -36,3 +36,48 @@ export const Primary: Story = {
     ],
   },
 };
+
+export const OpenSelect: Story = {
+  args: {
+    onChange: (value) => console.log(value),
+    value: "option1",
+    open: true,
+    options: [
+      {
+        label: "Option 1",
+        value: "option1",
+      },
+      {
+        label: "Option 2",
+        value: "option2",
+      },
+      {
+        label: "Option 3",
+        value: "option3",
+      },
+    ],
+  },
+};
+
+export const Error: Story = {
+  args: {
+    onChange: (value) => console.log(value),
+    value: "option1",
+    issue: "Bad value",
+    open: true,
+    options: [
+      {
+        label: "Option 1",
+        value: "option1",
+      },
+      {
+        label: "Option 2",
+        value: "option2",
+      },
+      {
+        label: "Option 3",
+        value: "option3",
+      },
+    ],
+  },
+};
--- packages/components/src/base/select.tsx
+++ packages/components/src/base/select.tsx
@@ -11,6 +11,7 @@ export interface SelectProps {
   onChange?: (value: Option) => void;
   issue?: string;
   label?: string;
+  open?: boolean;
 }
 
 export const Select = ({
@@ -18,6 +19,7 @@ export const Select = ({
   options,
   label,
   issue,
+  open = false,
   onChange,
 }: SelectProps) => {
   const [query, setQuery] = useState("");
@@ -34,8 +36,11 @@ export const Select = ({
 
   return (
     <Combobox
-      value={options.filter((o) => o.value === value)[0]}
+      value={
+        options.filter((o) => o.value === value)[0] || { value, label: value }
+      }
       onChange={onChange}
+      nullable
     >
       <div className="relative mt-1">
         <LabelContainer label={label} issues={issue ? [issue] : []}>
@@ -59,44 +64,50 @@ export const Select = ({
           afterLeave={() => setQuery("")}
         >
           <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {filteredOptions.length === 0 && query !== "" ? (
-              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                Nothing found.
-              </div>
-            ) : (
-              filteredOptions.map((option: Option) => (
-                <Combobox.Option
-                  key={option.value}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? "bg-teal-600 text-white" : "text-gray-900"
-                    }`
-                  }
-                  value={option}
-                >
-                  {({ selected, active }) => (
-                    <>
+            {query.length > 0 && open && (
+              <Combobox.Option
+                value={{ value: query, label: query }}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? "bg-teal-600 text-white" : "text-gray-900"
+                  }`
+                }
+              >
+                Create "{query}"
+              </Combobox.Option>
+            )}
+            {filteredOptions.map((option: Option) => (
+              <Combobox.Option
+                key={option.value}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? "bg-teal-600 text-white" : "text-gray-900"
+                  }`
+                }
+                value={option}
+              >
+                {({ selected, active }) => (
+                  <>
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.label}
+                    </span>
+                    {selected ? (
                       <span
-                        className={`block truncate ${
-                          selected ? "font-medium" : "font-normal"
+                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                          active ? "text-white" : "text-teal-600"
                         }`}
                       >
-                        {option.label}
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
                       </span>
-                      {selected ? (
-                        <span
-                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                            active ? "text-white" : "text-teal-600"
-                          }`}
-                        >
-                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Combobox.Option>
-              ))
-            )}
+                    ) : null}
+                  </>
+                )}
+              </Combobox.Option>
+            ))}
           </Combobox.Options>
         </Transition>
       </div>
