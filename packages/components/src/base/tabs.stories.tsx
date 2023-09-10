20e5bf78bc4a108c361bf9869da69932bd9f0630
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-09-09 19:18:55 -0500

    qalt

---
+++ packages/components/src/base/tabs.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Tabs } from "./tabs";
+
+const meta = {
+  title: "Base/Tabs",
+  component: Tabs,
+  tags: ["autodocs"],
+  parameters: {
+    ayout: "fullscreen",
+  },
+} satisfies Meta<typeof Tabs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    tabs: [
+      { id: 5, title: "Tab 1", content: "Tab 1 content" },
+      {
+        id: 6,
+        title: "Tab 2",
+        content: <span className="font-bold">Tab 2 content</span>,
+      },
+      {
+        id: 7,
+        title: "Tab 3",
+        content: <span className="font-medium underline">Tab 3 content</span>,
+      },
+    ],
+  },
+};
