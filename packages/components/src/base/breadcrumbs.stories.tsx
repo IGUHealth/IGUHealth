0fdc0a65bd7281b129b19cc88f80128568cd9bf4
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2023-09-10 11:45:03 -0500

    Breadcrumbs (#143)
    Co-authored-by: S O <9434934-karate-kid--@users.noreply.gitlab.com>

---
+++ packages/components/src/base/breadcrumbs.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { BreadCrumbs } from "./breadcrumbs";
+
+const meta = {
+  title: "Base/BreadCrumbs",
+  component: BreadCrumbs,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+} satisfies Meta<typeof BreadCrumbs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    breadcrumbs: [
+      <span>Home</span>,
+      <span>Resources</span>,
+      <span>Users</span>,
+    ],
+  },
+};
--- packages/components/src/base/toaster.tsx
+++ packages/components/src/base/toaster.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import toast, { Toaster } from "react-hot-toast";
+import toast, { Toaster, DefaultToastOptions } from "react-hot-toast";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
 
-export const notify = (message: string) => toast(message);
-export const loading = (message: string) => toast.loading(message);
-export const error = (message: string) => toast.error(message);
-export const success = (message: string) => toast.success(message);
+import { Loading } from "./loading";
+
+const defaultToastOptions: DefaultToastOptions = {
+  loading: {
+    icon: <Loading className="w-4 h-4" />,
+  },
+  success: {
+    icon: <CheckCircleIcon className="w-4 h-4 text-green-500" />,
+  },
+  error: {
+    icon: <XCircleIcon className="w-4 h-4 text-red-500" />,
+  },
+};
+
+export const notify = (...args: Parameters<typeof toast>) =>
+  toast(args[0], { ...defaultToastOptions, ...args[1] });
+
+export const loading = (...args: Parameters<typeof toast.loading>) =>
+  toast.loading(args[0], { ...defaultToastOptions, ...args[1] });
+
+export const error = (...args: Parameters<typeof toast.error>) =>
+  toast.error(args[0], { ...defaultToastOptions, ...args[1] });
+
+export const success = (...args: Parameters<typeof toast.success>) =>
+  toast.success(args[0], { ...defaultToastOptions, ...args[1] });
+
 export const promise = (...args: Parameters<typeof toast.promise>) =>
-  toast.promise(...args);
+  toast.promise(args[0], args[1], { ...defaultToastOptions, ...args[2] });
 
 export { Toaster };
