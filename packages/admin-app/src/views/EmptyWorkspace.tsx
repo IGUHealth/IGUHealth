5d36f55737c4b2d20b66c8477eb419c488b74010
S O <9434934-karate-kid--@users.noreply.gitlab.com>
2023-09-13 14:01:47 -0500

    alt

---
+++ packages/admin-app/src/views/EmptyWorkspace.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Base } from "@iguhealth/components";
+
+export default function EmptyWorkspace() {
+  const auth0 = useAuth0();
+  return (
+    <div className="h-screen w-screen flex  flex-col items-center">
+      <div className=" flex justify-center items-center flex-col px-4 py-4 shadow-md -top-[15px] mt-16">
+        <h1 className="text-xl font-semibold mb-4 ">
+          There are no workspaces associated with your account.
+        </h1>
+        <Base.Button type="secondary" onClick={(_e) => auth0.logout()}>
+          Logout
+        </Base.Button>
+      </div>
+    </div>
+  );
+}
