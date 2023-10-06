0664a4f23ecf813e2da5566d16fa8bad8557d089
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2023-10-06 16:02:12 -0500

    HumanName (#199)
    * alt
    
    * ver
    
    ---------
    
    Co-authored-by: S O <9434934-karate-kid--@users.noreply.gitlab.com>

---
+++ packages/components/src/fhir/complex/HumanName.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { HumanName } from "@iguhealth/fhir-types/r4/types";
+
+import { InputContainer } from "../../base/labelContainer";
+import { String } from "../primitives/string";
+
+export interface HumanNameEditableProps {
+  /**
+   * The value of the input.
+   */
+  value: HumanName | undefined;
+  /**
+   * Issues
+   */
+  issue?: string;
+  /**
+   * Call back triggered when input changes.
+   */
+  onChange?: (value: HumanName) => void;
+  /**
+   * Label string.
+   */
+  label?: string;
+}
+
+export const HumanNameEditable = ({
+  value,
+  onChange,
+  issue,
+  label,
+}: HumanNameEditableProps) => {
+  return (
+    <InputContainer label={label} issues={issue ? [issue] : []}>
+      <div className="flex space-x-1">
+        <String
+          label="First"
+          value={value?.given?.[0] || ""}
+          onChange={(firstName) => {
+            let given = value?.given ? [...value.given] : [];
+            given[0] = firstName;
+            onChange && onChange({ ...value, given });
+          }}
+        />
+        <String
+          label="Middle"
+          value={value?.given?.[1] || ""}
+          onChange={(middleName) => {
+            const given = value?.given ? [...value.given] : [];
+            if (!given[0]) given[0] = "";
+            given[1] = middleName;
+            onChange && onChange({ ...value, given });
+          }}
+        />
+        <String
+          label="Last"
+          value={value?.family}
+          onChange={(family) => {
+            onChange && onChange({ ...value, family });
+          }}
+        />
+      </div>
+    </InputContainer>
+  );
+};
--- packages/components/src/fhir/complex/index.ts
+++ packages/components/src/fhir/complex/index.ts
@@ -1,11 +1,13 @@
 import { AddressEditable } from "./Address";
 import { IdentifierEditable } from "./Identifier";
 import { ContactPointEditable } from "./ContactPoint";
+import { HumanNameEditable } from "./HumanName";
 import { MetaReadOnly } from "./Meta";
 
 export {
   AddressEditable,
   IdentifierEditable,
+  HumanNameEditable,
   MetaReadOnly,
   ContactPointEditable,
 };
