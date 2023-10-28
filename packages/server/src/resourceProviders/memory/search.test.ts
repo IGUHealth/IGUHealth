36cdcf5c077fa15cc3c924fc2aee62f441425595
confused-moniker <ralph@genfhi.app>
2023-10-28 15:51:40 -0500

    alterations

---
+++ packages/server/src/resourceProviders/memory/search.test.ts
@@ -0,0 +1,53 @@
+import path from "path";
+
+import {
+  ResourceType,
+  Resource,
+  Patient,
+} from "@iguhealth/fhir-types/r4/types";
+import { resourceTypes } from "@iguhealth/fhir-types/r4/sets";
+import { expect, test } from "@jest/globals";
+import { loadArtifacts } from "@iguhealth/artifacts";
+import * as fhirpath from "@iguhealth/fhirpath";
+import { FHIRClientAsync } from "@iguhealth/client/interface";
+
+import { testServices } from "../test_ctx.js";
+import MemoryDatabase from "./async.js";
+
+function createMemoryDatabase(
+  resourceTypes: ResourceType[]
+): FHIRClientAsync<unknown> {
+  const database = MemoryDatabase<unknown>({});
+  const artifactResources: Resource[] = resourceTypes
+    .map((resourceType) =>
+      loadArtifacts(resourceType, path.join(__dirname, "../../"), true)
+    )
+    .flat();
+  for (const resource of artifactResources) {
+    database.create({}, resource);
+  }
+  return database;
+}
+
+const memDB = createMemoryDatabase(["SearchParameter"] as ResourceType[]);
+
+const CTX = { ...testServices, client: memDB };
+
+test("TEST Name search", async () => {
+  await memDB.create(CTX, {
+    resourceType: "Patient",
+    id: "test",
+    name: [{ given: ["John"], family: "Doe" }],
+  });
+  const response = await memDB.search_type({}, "Patient", [
+    { name: "given", value: ["John"] },
+  ]);
+
+  expect(response.resources.map((r) => r.id)).toEqual(["test"]);
+
+  const response2 = await memDB.search_type({}, "Patient", [
+    { name: "given", value: ["John2"] },
+  ]);
+
+  expect(response2.resources.map((r) => r.id)).toEqual([]);
+});
