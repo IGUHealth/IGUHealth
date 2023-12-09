3d67dcddb794093515c7c84f1e1a9a8cf950d4ec
confused-moniker <ralph@genfhi.app>
2023-12-09 17:36:21 -0600

    q cleanup

--- packages/server/src/logging/auditEvents.ts
+++ packages/server/src/logging/auditEvents.ts
@@ -20,7 +20,6 @@ export default async function logAuditEvent(
   entity: Reference,
   outcomeDescription: string
 ) {
-  ctx.author;
   const auditEvent: AuditEvent = {
     resourceType: "AuditEvent",
     type: {
