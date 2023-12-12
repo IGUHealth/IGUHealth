cb99ed1bfd7a3f156f334e8cc65d6c8ee66e5c19
confused-moniker <ralph@genfhi.app>
2023-12-12 17:11:17 -0600

    simple quantity

---
+++ packages/components/src/fhir/complex/SimpleQuantity.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { FHIRSimpleQuantityEditable } from "./SimpleQuantity";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Complex/FHIRSimpleQuantityEditable",
+  component: FHIRSimpleQuantityEditable,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof FHIRSimpleQuantityEditable>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    value: {
+      unit: "km",
+      value: 55.2,
+    },
+    label: "Simple Quantity",
+    onChange: (value) => console.log(value),
+  },
+};
+
+export const OnError: Story = {
+  args: {
+    // @ts-ignore
+    value: "test",
+    label: "Bad value",
+    issue: "Bad value",
+    onChange: (value) => console.log(value),
+  },
+};
---
+++ packages/components/src/fhir/complex/SimpleQuantity.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Quantity } from "@iguhealth/fhir-types/r4/types";
+
+import { EditableProps } from "../types";
+import { InputContainer } from "../../base/containers";
+import { FHIRStringEditable, FHIRDecimalEditable } from "../primitives";
+
+export type FHIRSimpleQuantityEditableProps = EditableProps<Quantity>;
+
+export const FHIRSimpleQuantityEditable = ({
+  value,
+  onChange,
+  issue,
+  label,
+}: FHIRSimpleQuantityEditableProps) => {
+  return (
+    <InputContainer label={label} issues={issue ? [issue] : []}>
+      <div className="flex space-x-1">
+        <FHIRDecimalEditable
+          label="Value"
+          value={value?.value}
+          onChange={(valueDec) => {
+            onChange && onChange({ ...value, value: valueDec });
+          }}
+        />
+        <FHIRStringEditable
+          label="Unit"
+          value={value?.unit}
+          onChange={(unit) => {
+            onChange && onChange({ ...value, unit });
+          }}
+        />
+      </div>
+    </InputContainer>
+  );
+};
