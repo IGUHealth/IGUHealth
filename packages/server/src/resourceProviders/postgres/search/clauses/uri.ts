c8775d7090533bc2f6a17e064282b27956e97fde
confused-moniker <ralph@genfhi.app>
2023-12-24 23:53:05 -0600

    exp

--- packages/server/src/resourceProviders/postgres/search/clauses/uri.ts
+++ packages/server/src/resourceProviders/postgres/search/clauses/uri.ts
@@ -1,7 +1,17 @@
 import { FHIRServerCTX } from "../../../../ctx/types.js";
+import * as db from "zapatos/db";
+import type * as s from "zapatos/schema";
+
 import { or } from "../../../utilities/sql.js";
 import { SearchParameterResource } from "../../../utilities/search/parameters.js";
 import { FilterSQLResult } from "./types.js";
+
+const value = "5";
+
+const authors = await db.sql<s.uri_idx.SQL, s.uri_idx.Selectable[]>`
+  ${"value"} = ${db.vals(value)}`.compile();
+
+console.log(authors);
 
 export default function uriClauses(
   _ctx: FHIRServerCTX,
