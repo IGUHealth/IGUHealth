2d386b3f0c91856e213ceb4b51889b7259594396
confused-moniker <ralph@genfhi.app>
2023-12-25 18:49:41 -0600

    specify type in audit event

--- packages/server/src/logging/auditEvents.ts
+++ packages/server/src/logging/auditEvents.ts
@@ -44,6 +44,7 @@ export default async function logAuditEvent(
     ],
     source: {
       observer: {
+        type: "Device" as uri,
         identifier: {
           system: "https://iguhealth.com" as uri,
           value: "server",
