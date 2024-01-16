6a901426a528e9d4c6e4515caae9a346be132bd2
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-01-16 15:10:47 -0600

    Tenant Alts (#377)
    * alt
    
    * alt apis

--- packages/server/src/authZ/middleware/tenantAccess.ts
+++ packages/server/src/authZ/middleware/tenantAccess.ts
@@ -4,11 +4,11 @@ import { OperationError, outcomeFatal } from "@iguhealth/operation-outcomes";
 
 import type { FHIRServerCTX, JWT, TenantClaim } from "../../fhir/context.js";
 
-export function findCurrentTenant(
-  ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext>,
-): TenantClaim | undefined {
+function findCurrentTenant<
+  Context extends Koa.DefaultContext & { FHIRContext: FHIRServerCTX },
+>(ctx: Context): TenantClaim | undefined {
   return ctx.state.user["https://iguhealth.app/tenants"]?.find(
-    (t: TenantClaim) => t.id === ctx.params.tenant,
+    (t: TenantClaim) => t.id === ctx.FHIRContext.tenant,
   );
 }
 
@@ -16,15 +16,19 @@ export async function canUserAccessTenantMiddleware<
   State extends Koa.DefaultState,
   Context extends Koa.DefaultContext & { FHIRContext: FHIRServerCTX },
 >(ctx: Koa.ParameterizedContext<State, Context>, next: Koa.Next) {
-  if (!ctx.params.tenant) {
+  if (!ctx.FHIRContext.tenant) {
     ctx.throw(400, "tenant is required");
   }
-  if (findCurrentTenant(ctx) === undefined) {
+
+  const tenantClaim = findCurrentTenant(ctx);
+
+  if (tenantClaim === undefined) {
     ctx.throw(
       403,
       `User is not authorized to access tenant ${ctx.params.tenant}`,
     );
   }
+
   if (
     typeof ctx.state.user.sub !== "string" ||
     typeof ctx.state.user.iss !== "string"
@@ -33,13 +37,10 @@ export async function canUserAccessTenantMiddleware<
       outcomeFatal("security", "JWT must have both sub and iss."),
     );
 
-  const tenant = findCurrentTenant(ctx);
-  if (!tenant) throw new Error("Error tenant does not exist in context!");
   ctx.FHIRContext = {
     ...ctx.FHIRContext,
-    tenant: tenant.id,
     user: {
-      role: tenant.userRole,
+      role: tenantClaim.userRole,
       jwt: ctx.state.user as JWT,
       accessToken: ctx.state.access_token,
     },
