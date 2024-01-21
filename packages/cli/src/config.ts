17f0e91819cffc5e1b01377d0ddacabf768b912f
confused-moniker <ralph@genfhi.app>
2024-01-21 12:48:58 -0600

    init

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,12 +1,13 @@
 import toml, { JsonMap } from "@iarna/toml";
+import * as inquirer from "@inquirer/prompts";
 import { Command } from "commander";
 import fs from "node:fs";
 import { homedir } from "node:os";
 import path from "node:path";
 
-const CONFIG_LOCATION = path.join(homedir(), ".iguhealth/config.toml");
+export const CONFIG_LOCATION = path.join(homedir(), ".iguhealth/config.toml");
 
-interface Tenant extends JsonMap {
+export interface Tenant extends JsonMap {
   id: string;
   name: string;
   auth: {
@@ -16,24 +17,88 @@ interface Tenant extends JsonMap {
   };
 }
 
-interface Config extends JsonMap {
+export interface Config extends JsonMap {
+  api_url: string;
   lastUpdated: string;
+  defaultTenant: Tenant["id"];
   tenants: Tenant[];
+}
+
+export function loadConfig(location: string): Config {
+  const configuration = fs.readFileSync(location, "utf8");
+  return toml.parse(configuration) as Config;
+}
+
+export function saveConfig(location: string, config: Config) {
+  if (!fs.existsSync(path.join(location, "../"))) {
+    fs.mkdirSync(path.join(location, "../"), { recursive: true });
+  }
+  fs.writeFileSync(location, toml.stringify(config), "utf8");
+}
+
+export function getCurrentTenant(
+  location: string,
+  config: Config,
+): Tenant | undefined {
+  if (!config.defaultTenant) {
+    config = {
+      ...config,
+      defaultTenant: config.tenants[0]?.id,
+    };
+    saveConfig(location, config);
+  }
+
+  return config.tenants.find((tenant) => tenant.id === config.defaultTenant);
+}
+
+async function initializeConfig(location: string) {
+  console.log("Initializing configuration file");
+
+  const initialConfig: Config = {
+    api_url: await inquirer.input({
+      message: "Root API URL:",
+      default: "https://api.iguhealth.app",
+    }),
+    defaultTenant: "",
+    lastUpdated: new Date().toISOString(),
+    tenants: [],
+  };
+
+  saveConfig(location, initialConfig);
 }
 
 export function configurationCommands(command: Command) {
   command
     .command("init")
     .description("Initializes the configuration file")
-    .action(() => {
+    .action(async () => {
       console.log("Initializing configuration file");
-      const initialConfig: Config = {
-        lastUpdated: new Date().toISOString(),
-        tenants: [],
-      };
-      if (!fs.existsSync(path.join(CONFIG_LOCATION, "../"))) {
-        fs.mkdirSync(path.join(CONFIG_LOCATION, "../"), { recursive: true });
-      }
-      fs.writeFileSync(CONFIG_LOCATION, toml.stringify(initialConfig), "utf8");
+      await initializeConfig(CONFIG_LOCATION);
+    });
+
+  command
+    .command("add-tenant")
+    .description("Add a tenant to the configuration file")
+    .action(async () => {
+      const id = await inquirer.input({ message: "Enter tenant id:" });
+      const name = await inquirer.input({ message: "Enter tenant name:" });
+      const client_id = await inquirer.input({
+        message: "Enter client id:",
+      });
+      const client_secret = await inquirer.input({
+        message: "Enter client secret:",
+      });
+      const config = loadConfig(CONFIG_LOCATION);
+
+      config.tenants.push({
+        id,
+        name,
+        auth: {
+          type: "client_credentials",
+          client_id,
+          client_secret,
+        },
+      });
+      saveConfig(CONFIG_LOCATION, config);
     });
 }
