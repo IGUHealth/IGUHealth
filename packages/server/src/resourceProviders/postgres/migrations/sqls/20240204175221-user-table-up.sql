f246b502ed2d6e51acacc101c5d1356d898c9e66
confused-moniker <ralph@genfhi.app>
2024-02-05 09:29:35 -0600

    alt

--- packages/server/src/resourceProviders/postgres/migrations/sqls/20240204175221-user-table-up.sql
+++ packages/server/src/resourceProviders/postgres/migrations/sqls/20240204175221-user-table-up.sql
@@ -3,14 +3,21 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS tenant_owners  (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  -- Login credentials
   email TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL,
+  -- Personal information
+  first_name TEXT,
+  last_name TEXT,
+  phone_number TEXT,
+  -- System fields
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 --;;
 
+-- Encryption logic to auto-encrypt password column.
 CREATE FUNCTION user_password_encryption() RETURNS trigger AS $user_password_encryption$
     BEGIN
         NEW.password := crypt(NEW.password, gen_salt('bf'));
@@ -20,5 +27,6 @@ $user_password_encryption$ LANGUAGE plpgsql;
 
 --;;
 
+-- Associate encryption logic with update trigger.
 CREATE TRIGGER user_password_encryption_trigger BEFORE INSERT OR UPDATE ON tenant_owners
     FOR EACH ROW EXECUTE FUNCTION user_password_encryption();
