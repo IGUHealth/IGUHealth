22c819bb0d335cb4041879d124e98edfcf3c5c99
confused-moniker <ralph@genfhi.app>
2024-02-05 10:20:29 -0600

    alt

--- packages/server/src/resourceProviders/postgres/migrations/sqls/20240204175221-user-table-up.sql
+++ packages/server/src/resourceProviders/postgres/migrations/sqls/20240204175221-user-table-up.sql
@@ -20,7 +20,11 @@ CREATE TABLE IF NOT EXISTS tenant_owners  (
 -- Encryption logic to auto-encrypt password column.
 CREATE FUNCTION user_password_encryption() RETURNS trigger AS $user_password_encryption$
     BEGIN
-        NEW.password := crypt(NEW.password, gen_salt('bf'));
+        -- Null check does not work for equality operators.
+        IF NEW.password IS DISTINCT FROM OLD.password THEN
+          NEW.password := crypt(NEW.password, gen_salt('bf'));
+        END IF;
+
         RETURN NEW;
     END;
 $user_password_encryption$ LANGUAGE plpgsql;
