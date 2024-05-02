900f91928d949d42b654d1c51768cc8dab57ce3f
confused-moniker <ralph@genfhi.app>
2024-05-02 14:21:08 -0500

    init

--- packages/fhir-types/src/generated/r4/types.ts
+++ packages/fhir-types/src/generated/r4/types.ts
@@ -4387,6 +4387,7 @@ export type ResourceMap = {
   AccessPolicy: AccessPolicy;
   ClientApplication: ClientApplication;
   Membership: Membership;
+  MessageBroker: MessageBroker;
 }
 
 export type ResourceType = keyof ResourceMap
@@ -45119,4 +45120,70 @@ resourceType: "Membership"
    * The Memberships role. Note that owner and superadmin roles are granted full access.
    */
   _role?: Element
+}
+
+export interface MessageBrokerSecurity {
+  /** 
+   * The type of message broker security.
+   */
+  type: code;
+  /** 
+   * The type of message broker security.
+   */
+  _type?: Element
+  /** 
+   * Username for the message broker security.
+   */
+  username?: string;
+  /** 
+   * Username for the message broker security.
+   */
+  _username?: Element
+  /** 
+   * Password for the message broker security.
+   */
+  password?: string;
+  /** 
+   * Password for the message broker security.
+   */
+  _password?: Element
+}
+export interface MessageBroker {
+resourceType: "MessageBroker"
+  /** 
+   * Logical id of this artifact
+   */
+  id?: id;
+  /** 
+   * Metadata about the resource
+   */
+  meta?: Meta;
+  /** 
+   * Name of the message broker.
+   */
+  name: string;
+  /** 
+   * Name of the message broker.
+   */
+  _name?: Element
+  /** 
+   * The host for the message broker.
+   */
+  host: uri;
+  /** 
+   * The host for the message broker.
+   */
+  _host?: Element
+  /** 
+   * The type of message broker.
+   */
+  type: code;
+  /** 
+   * The type of message broker.
+   */
+  _type?: Element
+  /** 
+   * MessageBroker security.
+   */
+  security: MessageBrokerSecurity;
 }
