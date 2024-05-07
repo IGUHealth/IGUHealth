e85acc40ac1b14cbd8a97a3822ed72604d15e9f5
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-05-07 15:53:14 -0500

    Algolia Docs (#567)

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -63,6 +63,38 @@ const config = {
         autoCollapseCategories: false,
       },
     },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "9M3PZB2S4M",
+
+      // Public API key: it is safe to commit it
+      apiKey: "c1d17934425156e91976e6db786b4117",
+
+      indexName: "iguhealth",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: "external\\.com|domain\\.com",
+
+      // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: "/docs/", // or as RegExp: /\/docs\//
+      //   to: "/",
+      // },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
     navbar: {
       title: "IGUHealth",
       logo: {
