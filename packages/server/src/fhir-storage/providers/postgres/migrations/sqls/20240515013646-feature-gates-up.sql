2b0114c5ba089c4b32cd13623a68948572a6436f
confused-moniker <ralph@genfhi.app>
2024-05-15 12:54:20 -0500

    client app gate

--- packages/server/src/fhir-storage/providers/postgres/migrations/sqls/20240515013646-feature-gates-up.sql
+++ packages/server/src/fhir-storage/providers/postgres/migrations/sqls/20240515013646-feature-gates-up.sql
@@ -50,6 +50,7 @@ INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tie
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-free-messagetopic-total',          'R4 Free MessageTopic Limitation',         'r4',  'LIMIT_TOTAL', 'MessageTopic', 0, 'free');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-free-messagebroker-total',         'R4 Free MessageBroker Limitation',        'r4',  'LIMIT_TOTAL', 'MessageBroker', 0, 'free');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-free-membership-total',            'R4 Free Membership Limitation',           'r4',  'LIMIT_TOTAL', 'Membership', 5, 'free');
+INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-free-client-application-total',    'R4 Free ClientApplication Limitation',    'r4',  'LIMIT_TOTAL', 'ClientApplication', 5, 'free');
 
 -- R4B
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4b-free-total',                       'R4B Free Total Limitation',              'r4b', 'LIMIT_TOTAL', 'ALL', 3000, 'free');
@@ -67,6 +68,7 @@ INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tie
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-professional-messagetopic-total',          'R4 Professional MessageTopic Limitation',        'r4',   'LIMIT_TOTAL', 'MessageTopic', 5, 'professional');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-professional-messagebroker-total',         'R4 Professional MessageBroker Limitation',       'r4',   'LIMIT_TOTAL', 'MessageBroker', 2, 'professional');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-professional-membership-total',            'R4 Professional Membership Limitation',          'r4',   'LIMIT_TOTAL', 'Membership', 100, 'professional');
+INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-professional-client-application-total',    'R4 Professional ClientApplication Limitation',   'r4',   'LIMIT_TOTAL', 'ClientApplication', 50, 'professional');
 -- R4B
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4b-professional-total',                      'R4B Professional Total Limitation',               'r4b', 'LIMIT_TOTAL', 'ALL', 50000, 'professional');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4b-professional-operation-definition-total', 'R4B Professional OperationDefinition Limitation', 'r4b', 'LIMIT_TOTAL', 'OperationDefinition', 5, 'professional');
@@ -84,6 +86,7 @@ INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tie
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-team-messagetopic-total',          'R4 Team MessageTopic Limitation',         'r4',  'LIMIT_TOTAL', 'MessageTopic', 50, 'team');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-team-messagebroker-total',         'R4 Team MessageBroker Limitation',        'r4',  'LIMIT_TOTAL', 'MessageBroker', 10, 'team');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-team-membership-total',            'R4 Team Membership Limitation',           'r4',   'LIMIT_TOTAL', 'Membership', 5000, 'team');
+INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4-team-client-application-total',    'R4 Team ClientApplication Limitation',    'r4',   'LIMIT_TOTAL', 'ClientApplication', 500, 'team');
 -- R4B
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4b-team-total',                      'R4B Team Total Limitation',               'r4b', 'LIMIT_TOTAL', 'ALL', 100000, 'team');
 INSERT INTO limitations (id, name, fhir_version, type, resource_type, value, tier) VALUES ('r4b-team-operation-definition-total', 'R4B Team OperationDefinition Limitation', 'r4b', 'LIMIT_TOTAL', 'OperationDefinition', 30, 'team');
