011e84d50733a7bc6359148e1ef68beb60f570f9
confused-moniker <ralph@genfhi.app>
2024-05-15 15:07:42 -0500

    alt

--- packages/docs/src/components/Card.tsx
+++ packages/docs/src/components/Card.tsx
@@ -1,31 +1,84 @@
-interface CardProps {
-  title: string;
-  price: number;
-  description: string;
-  features: string[];
+function CheckMark({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
 }
 
-export default function Card({ title, price, description, features }) {
+interface CardProps {
+  title: string;
+  price: string;
+  priceSuffix?: string;
+  description: string;
+  features?: string[];
+  support?: string[];
+}
+
+export default function Card({
+  title,
+  price,
+  priceSuffix,
+  description,
+  features,
+  support,
+}: CardProps) {
   return (
-    <div className="w-full px-4 md:w-1/2 lg:w-1/4">
-      <div className="relative z-10 mb-10 overflow-hidden rounded-[10px] border-solid border-2 border-stroke dark:border-dark-3 bg-white dark:bg-dark-2 py-10 px-8 shadow-pricing sm:p-12 lg:py-10 lg:px-6 xl:p-[50px]">
+    <div className="w-full px-4 md:w-1/2 xl:w-1/4">
+      <div className="h-[900px] relative z-10 mb-10 overflow-hidden rounded-[10px] border-solid border-2 border-stroke dark:border-dark-3 bg-white dark:bg-dark-2 py-10 px-8 shadow-pricing sm:p-12 lg:py-10 lg:px-6 xl:p-[50px]">
         <span className="mb-3 block text-lg font-semibold text-primary">
           {title}
         </span>
         <h2 className="mb-5 text-[42px] font-bold text-dark dark:text-white">
-          <span>${price}</span>
-          <span className="text-base font-medium text-body-color dark:text-dark-6">
-            / month
-          </span>
+          <span>{price}</span>
+          {priceSuffix && (
+            <span className="text-base font-medium text-body-color dark:text-dark-6">
+              {priceSuffix}
+            </span>
+          )}
         </h2>
-        <p className="mb-8 border-b border-stroke dark:border-dark-3 pb-8 text-base text-body-color dark:text-dark-6">
+        <p className="mb-2 border-b border-stroke pb-8 text-base text-body-color dark:text-dark-6 dark:border-dark-3">
           {description}
         </p>
-        <div className="mb-9 flex flex-col space-y-2">
-          {features.map((feature) => (
-            <p className="mb-0 text-base text-body-color dark:text-dark-6">
-              {feature}
-            </p>
+
+        <h3>Features</h3>
+
+        <div className="mb-9 flex flex-col space-y-4">
+          {features?.map((feature) => (
+            <div className="flex items-center">
+              <div className="w-4 h-4 flex items-center justify-center mr-2">
+                <CheckMark className="w-4" />
+              </div>
+              <span className="font-weight-500 text-sm text-body-color dark:text-dark-6">
+                {feature}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <h3>Support</h3>
+
+        <div className="mb-9 flex flex-col space-y-4">
+          {support?.map((feature) => (
+            <div className="flex items-center">
+              <div className="w-4 h-4 flex items-center justify-center mr-2">
+                <CheckMark className="w-4" />
+              </div>
+              <span className="font-weight-500 text-sm text-body-color dark:text-dark-6">
+                {feature}
+              </span>
+            </div>
           ))}
         </div>
       </div>
--- packages/docs/src/pages/pricing.tsx
+++ packages/docs/src/pages/pricing.tsx
@@ -18,40 +18,130 @@ export default function Home(): JSX.Element {
                     Pricing Table
                   </span>
                   <h2 className="mb-3 text-3xl leading-[1.208] font-bold text-dark dark:text-white sm:text-4xl md:text-[40px]">
-                    Our Pricing Plan
+                    Our Pricing Plans
                   </h2>
                   <p className="text-base text-body-color dark:text-dark-6">
-                    There are many variations of passages of Lorem Ipsum
-                    available but the majority have suffered alteration in some
-                    form.
+                    These are our pricing plans. We support a wide range of
+                    deployment options such as cloud, on-premises, hybrid.
+                    Contact us for more information.
                   </p>
                 </div>
               </div>
             </div>
             <div className="-mx-4 flex flex-wrap justify-center">
               <Card
-                title="Self Hosting"
-                price={0}
-                description="Test"
-                features={["TEsting", "testing2"]}
+                title="Free"
+                price="$0"
+                priceSuffix="/ month"
+                description=""
+                features={[
+                  "3,000 FHIR R4 Resources",
+                  "3,000 FHIR R4B Resources",
+                  "5 Users",
+                  "5 Client Applications",
+                  "0 Message Brokers",
+                  "0 Message Topics",
+                  "0 R4 Custom Operations",
+                  "0 R4 Subscriptions",
+                  "0 R4B Custom Operations",
+                  "0 R4B Subscriptions",
+                ]}
+                support={[
+                  "Support via Github Issues (No guarantees on response time)",
+                  "No assistance for deploying custom code/solutions",
+                  "Cloud deployment only",
+                ]}
               />
               <Card
                 title="Professional"
-                price={120}
-                description="Test"
-                features={["TEsting", "testing2"]}
+                price="$100"
+                priceSuffix="/ month"
+                description=""
+                features={[
+                  "50,000 FHIR R4 Resources",
+                  "50,000 FHIR R4B Resources",
+                  "100 Users",
+                  "50 Client Applications",
+                  "2 Message Brokers",
+                  "5 Message Topics",
+                  "5 R4 Custom Operations",
+                  "5 R4 Subscriptions",
+                  "5 R4B Custom Operations",
+                  "5 R4B Subscriptions",
+                ]}
+                support={[
+                  "Support via email (Response time: 24 hours)",
+                  "No assistance for deploying custom code/solutions",
+                  "Cloud deployment only",
+                ]}
               />
               <Card
                 title="Team"
-                price={3000}
-                description="Test"
-                features={["TEsting", "testing2"]}
+                price="$3,000"
+                priceSuffix="/ month"
+                description=""
+                features={[
+                  "100,000 FHIR R4 Resources",
+                  "100,000 FHIR R4B Resources",
+                  "5000 Users",
+                  "500 Client Applications",
+                  "10 Message Brokers",
+                  "50 Message Topics",
+                  "30 R4 Custom Operations",
+                  "30 R4 Subscriptions",
+                  "30 R4B Custom Operations",
+                  "30 R4B Subscriptions",
+                ]}
+                support={[
+                  "24 Hour Support",
+                  "Personal assistance with deploying custom code/solutions",
+                  "Cloud deployment only",
+                ]}
               />
               <Card
                 title="Enterprise"
-                price={5000}
-                description="Test"
-                features={["TEsting", "testing2"]}
+                price="Contact us"
+                description=""
+                features={[
+                  "Unlimited FHIR R4 Resources",
+                  "Unlimited FHIR R4B Resources",
+                  "Unlimited Users",
+                  "Unlimited Client Applications",
+                  "Unlimited Message Brokers",
+                  "Unlimited Message Topics",
+                  "Unlimited R4 Custom Operations",
+                  "Unlimited R4 Subscriptions",
+                  "Unlimited R4B Custom Operations",
+                  "Unlimited R4B Subscriptions",
+                ]}
+                support={[
+                  "24 Hour Support",
+                  "Personal assistance with deploying code/solutions",
+                  "On Premise Deployment",
+                ]}
+              />
+              <Card
+                title="Self-hosting"
+                price="$0"
+                priceSuffix="/ month"
+                description=""
+                features={[
+                  "Unlimited FHIR R4 Resources",
+                  "Unlimited FHIR R4B Resources",
+                  "Unlimited Users",
+                  "Unlimited Client Applications",
+                  "Unlimited Message Brokers",
+                  "Unlimited Message Topics",
+                  "Unlimited R4 Custom Operations",
+                  "Unlimited R4 Subscriptions",
+                  "Unlimited R4B Custom Operations",
+                  "Unlimited R4B Subscriptions",
+                ]}
+                support={[
+                  "Support via Github Issues (No guarantees on response time)",
+                  "No assistance for deploying custom code/solutions",
+                  "Cloud deployment only",
+                ]}
               />
             </div>
           </div>
