4b20a89d4b8578b93f49c8a7f6d65931cccb3614
confused-moniker <ralph@genfhi.app>
2024-05-21 12:07:15 -0500

    alt

--- packages/admin-app/src/components/Card.tsx
+++ packages/admin-app/src/components/Card.tsx
@@ -15,7 +15,7 @@ export default function Card({
 }: Readonly<CardProps>) {
   return (
     <div className="text-slate-800 w-full px-4 md:w-1/2 xl:w-1/4">
-      <div className="h-[300px] relative z-10 mb-10 overflow-hidden rounded-[10px] border-solid border-2 border-stroke  py-10 px-8 shadow-pricing sm:p-12 lg:py-10 lg:px-6 xl:p-[50px]">
+      <div className="h-[300px] relative mb-10 overflow-hidden rounded-[10px] border-solid border-2 border-stroke  py-10 px-8 shadow-pricing sm:p-12 lg:py-10 lg:px-6 xl:p-[50px]">
         <span className="mb-6 block text-lg font-semibold text-primary truncate">
           {title}
         </span>
