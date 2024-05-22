a7fa18751850a2181b55ce7a732154427ea78bb7
confused-moniker <ralph@genfhi.app>
2024-05-21 20:20:16 -0500

    dropdown as div

--- packages/components/src/base/menu.tsx
+++ packages/components/src/base/menu.tsx
@@ -19,7 +19,7 @@ function DropDownMenu({ links, children }: DropdownMenuProps) {
   return (
     <Menu>
       <div>
-        <Menu.Button>{children}</Menu.Button>
+        <Menu.Button as="div">{children}</Menu.Button>
         <div className="relative">
           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             {links.map((link) => (
