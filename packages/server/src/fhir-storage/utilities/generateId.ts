735240ec628b7bc23d1d25bf8293e1f9813e7748
confused-moniker <ralph@genfhi.app>
2024-05-23 21:21:45 -0500

    alt

--- packages/server/src/fhir-storage/utilities/generateId.ts
+++ packages/server/src/fhir-storage/utilities/generateId.ts
@@ -3,6 +3,7 @@ import { customAlphabet } from "nanoid";
 import { id } from "@iguhealth/fhir-types/lib/generated/r4/types";
 
 // [A-Za-z0-9\-\.]{1,64}
+// Can't use _ for compliance.
 const _generateId = customAlphabet(
   "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-",
   22,
