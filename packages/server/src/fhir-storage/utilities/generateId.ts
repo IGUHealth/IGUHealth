c72fa3d7764bafcb9d88d172eeeb47324ee35cde
confused-moniker <ralph@genfhi.app>
2024-05-23 21:24:56 -0500

    include reference

--- packages/server/src/fhir-storage/utilities/generateId.ts
+++ packages/server/src/fhir-storage/utilities/generateId.ts
@@ -2,7 +2,7 @@ import { customAlphabet } from "nanoid";
 
 import { id } from "@iguhealth/fhir-types/lib/generated/r4/types";
 
-// [A-Za-z0-9\-\.]{1,64}
+// [A-Za-z0-9\-\.]{1,64} See https://hl7.org/fhir/r4/datatypes.html#id
 // Can't use _ for compliance.
 const _generateId = customAlphabet(
   "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-",
