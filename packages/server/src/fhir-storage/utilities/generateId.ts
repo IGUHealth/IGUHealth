86491eb5b365d133062c319edac6d25102f0b001
confused-moniker <ralph@genfhi.app>
2024-05-23 21:21:29 -0500

    alt

---
+++ packages/server/src/fhir-storage/utilities/generateId.ts
@@ -0,0 +1,13 @@
+import { customAlphabet } from "nanoid";
+
+import { id } from "@iguhealth/fhir-types/lib/generated/r4/types";
+
+// [A-Za-z0-9\-\.]{1,64}
+const _generateId = customAlphabet(
+  "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-",
+  22,
+);
+
+export function generateId(): id {
+  return _generateId() as id;
+}
