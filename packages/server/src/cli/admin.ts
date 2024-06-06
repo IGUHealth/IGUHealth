931811deb9a87a1f6edccf78027afc9060fe6032
confused-moniker <ralph@genfhi.app>
2024-06-06 10:57:58 -0500

    alt

--- packages/server/src/cli/admin.ts
+++ packages/server/src/cli/admin.ts
@@ -1,7 +1,65 @@
+import * as inquirer from "@inquirer/prompts";
 import { Command } from "commander";
+import validator from "validator";
+import * as db from "zapatos/db";
 
+import { Membership, code } from "@iguhealth/fhir-types/r4/types";
+import { R4 } from "@iguhealth/fhir-types/versions";
+import { TenantId } from "@iguhealth/jwt";
+
+import { TenantManagement } from "../authN/db/tenant.js";
+import TenantUserManagement from "../authN/db/users/index.js";
 import { createFHIRServices } from "../fhir-api/index.js";
+import { FHIRServerCTX, asSystemCTX } from "../fhir-api/types.js";
 import { createPGPool } from "../fhir-storage/providers/postgres/pg.js";
+import { FHIRTransaction } from "../fhir-storage/transactions.js";
+
+async function createTenant(ctx: Omit<FHIRServerCTX, "tenant" | "user">) {
+  return await FHIRTransaction(
+    ctx,
+    db.IsolationLevel.Serializable,
+    async (ctx) => {
+      const tenantManagement = new TenantManagement();
+
+      const tenant = await tenantManagement.create(ctx, {});
+
+      const email = await inquirer.input({
+        message: "Enter root user email.",
+        validate: (input) => {
+          return validator.isEmail(input);
+        },
+      });
+
+      const password = await inquirer.password({
+        message: "Enter root user password.",
+        validate: (input) => {
+          return validator.isStrongPassword(input);
+        },
+      });
+
+      const membership: Membership = await ctx.client.create(
+        asSystemCTX({ ...ctx, tenant: tenant.id as TenantId }),
+        R4,
+        { resourceType: "Membership", email, role: "owner" as code },
+      );
+
+      const userManagement = new TenantUserManagement(tenant.id as TenantId);
+
+      const user = await userManagement.search(ctx, {
+        fhir_user_id: membership.id as string,
+      });
+
+      await userManagement.update(ctx, user[0].id, {
+        ...user[0],
+        password,
+        email_verified: true,
+      });
+
+      console.log(`Tenant created with id: '${tenant.id}'`);
+      console.log(`User created with email: '${user[0].email}'`);
+    },
+  );
+}
 
 function tenantCommands(command: Command) {
   command
@@ -10,6 +68,9 @@ function tenantCommands(command: Command) {
     .action(async () => {
       const pool = createPGPool();
       const services = await createFHIRServices(pool);
+      await createTenant(services);
+
+      process.exit(0);
     });
 }
 
