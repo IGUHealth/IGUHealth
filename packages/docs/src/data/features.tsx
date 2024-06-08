eadf77a1088d09d9763344812599a7ffaf3dcd41
confused-moniker <ralph@genfhi.app>
2024-06-08 14:41:45 -0500

    typo

--- packages/docs/src/data/features.tsx
+++ packages/docs/src/data/features.tsx
@@ -30,10 +30,9 @@ const FEATURES: FeatureItem[] = [
     },
     text: (
       <Translate>
-        Build on top of the FHIR standard, IGUHealth is FHIR native. Create your
-        own platform that allows you to manage, store, and exchange healthcare
-        data in a secure, scalable, and compliant way. We support FHIR R4 and
-        R4B resources.
+        Build on top of the FHIR standard. Create your own platform that allows
+        you to manage, store, and exchange healthcare data in a secure,
+        scalable, and compliant way. We support FHIR R4 and R4B resources.
       </Translate>
     ),
   },
