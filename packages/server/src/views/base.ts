ea49645d9eda275486e567f85d77a2581f077c6d
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-06-08 18:55:36 -0500

    Remove style inline in favor of @import (#661)
    * init
    
    * rm

--- packages/server/src/views/base.ts
+++ packages/server/src/views/base.ts
@@ -1,11 +1,4 @@
-import { createRequire } from "module";
-import fs from "node:fs";
 import React from "react";
-
-const require = createRequire(import.meta.url);
-const indexCSS = fs
-  .readFileSync(require.resolve("@iguhealth/components/dist/index.css"))
-  .toString();
 
 export default function Base({
   title = "IGUHealth",
@@ -30,9 +23,6 @@ export default function Base({
       React.createElement("link", {
         rel: "stylesheet",
         href: "/public/css/index.css",
-      }),
-      React.createElement("style", {
-        dangerouslySetInnerHTML: { __html: indexCSS },
       }),
     ),
     React.createElement("body", null, children),
