e58df0ee49e44da311fa65e767983ccd7f1a7638
confused-moniker <ralph@genfhi.app>
2024-06-09 15:45:45 -0500

    alt

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -107,6 +107,105 @@ export default function Pricing(): JSX.Element {
       <section className="relative z-10 overflow-hidden  pt-10 pb-12">
         <HeroBanner />
         <FeaturesContainer />
+        <div className="mt-8 mb-24">
+          <div className="py-24 -skew-y-6 bg-gray-200 overflow-hidden">
+            <div className="skew-y-6 max-w-xl mx-auto px-4 md:px-6 lg:px-8 lg:max-w-screen-xl">
+              <div className="row">
+                <div className="col flex flex-col justify-center items-center">
+                  <div className="sm:w-full row">
+                    <div className="flex flex-col col px-8 mb-8 pt-12 text-gray-700">
+                      <h1 className=" font-bold ">Schedule a demo</h1>
+                      <span className=" block text-lg font-weight-500">
+                        Do you want to learn more? Schedule a 1:1 session with
+                        our team to see how we can help.
+                      </span>
+                      <ul className="mt-4 space-y-2 ">
+                        <li>
+                          Create an interoperable platform for your
+                          organization.
+                        </li>
+                        <li>Build a custom solutions for your organization.</li>
+                        <li>
+                          Consultation about FHIR, Hl7v2 and other healthcare
+                          standards.
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="flex flex-col p-4 col bg-gray-50 rounded-md shadow-lg items-center border border-solid space-y-4 border-gray-400">
+                      <form className="w-full max-w-[500px] py-8 space-y-4">
+                        <div className="flex flex-wrap space-y-4 md:space-y-0">
+                          <div className="w-full md:w-1/2 px-3 md:mb-0 ">
+                            <label
+                              className="block tracking-wide text-gray-700 text-xs font-bold mb-2"
+                              for="grid-first-name"
+                            >
+                              First name
+                            </label>
+                            <input
+                              className="appearance-none block w-full bg-gray-100 text-gray-700 border-solid border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                              id="grid-first-name"
+                              type="text"
+                            />
+                          </div>
+                          <div className="w-full md:w-1/2 px-3">
+                            <label
+                              className="block tracking-wide text-gray-700 text-xs font-bold mb-2"
+                              for="grid-last-name"
+                            >
+                              Last name
+                            </label>
+                            <input
+                              className="appearance-none block w-full bg-gray-100 text-gray-700 border-solid border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                              id="grid-last-name"
+                              type="text"
+                            />
+                          </div>
+                        </div>
+                        <div className="flex flex-wrap">
+                          <div className="w-full px-3 md:mb-0">
+                            <label
+                              className="block  tracking-wide text-gray-700 text-xs font-bold mb-2"
+                              for="grid-email"
+                            >
+                              Email address
+                            </label>
+                            <input
+                              className="appearance-none block w-full bg-gray-100 text-gray-700 border-solid border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                              id="grid-email"
+                              type="text"
+                            />
+                          </div>
+                        </div>
+                        <div className="flex flex-wrap">
+                          <div className="w-full px-3">
+                            <label
+                              className="block tracking-wide text-gray-700 text-xs font-bold mb-2"
+                              for="grid-body"
+                            >
+                              Body
+                            </label>
+                            <textarea
+                              className="appearance-none block w-full bg-gray-100 text-gray-700 border-solid border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                              id="grid-body"
+                            />
+                          </div>
+                        </div>
+                        <div className="px-3">
+                          <button
+                            type="submit"
+                            className="mt-4 border border-solid border-blue-700 bg-blue-500 py-3 px-5 text-sm font-medium text-center text-white rounded bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
+                          >
+                            Book demo
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </Layout>
   );
