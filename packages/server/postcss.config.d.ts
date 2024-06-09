88a93d86585be40535e9a956b83529c1a9a0bcfd
confused-moniker <ralph@genfhi.app>
2024-06-08 19:07:30 -0500

    ignore postcss

---
+++ packages/server/postcss.config.d.ts
@@ -0,0 +1,9 @@
+declare namespace _default {
+    let plugins: {
+        "postcss-import": {};
+        tailwindcss: {};
+        autoprefixer: {};
+        cssnano: {};
+    };
+}
+export default _default;
