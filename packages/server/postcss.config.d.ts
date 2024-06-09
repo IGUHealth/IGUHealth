4bbe8cca15cac00fe2cad59ff48e0bd8a4cccf9f
confused-moniker <ralph@genfhi.app>
2024-06-08 19:43:42 -0500

    alt typing on postcss

--- packages/server/postcss.config.d.ts
+++ packages/server/postcss.config.d.ts
@@ -1,9 +1,9 @@
 declare namespace _default {
-    let plugins: {
-        "postcss-import": {};
-        tailwindcss: {};
-        autoprefixer: {};
-        cssnano: {};
-    };
+  let plugins: {
+    "postcss-import": Record<string, unknown>;
+    tailwindcss: Record<string, unknown>;
+    autoprefixer: Record<string, unknown>;
+    cssnano: Record<string, unknown>;
+  };
 }
 export default _default;
