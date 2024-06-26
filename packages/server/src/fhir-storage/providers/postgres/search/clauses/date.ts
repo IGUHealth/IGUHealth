acf604f90946320002321c121fa6d66de504d75d
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-06-26 14:05:57 -0500

    Date range Tests (#703)

--- packages/server/src/fhir-storage/providers/postgres/search/clauses/date.ts
+++ packages/server/src/fhir-storage/providers/postgres/search/clauses/date.ts
@@ -2,15 +2,37 @@ import dayjs from "dayjs";
 import * as db from "zapatos/db";
 import type * as s from "zapatos/schema";
 
+import { date, dateTime } from "@iguhealth/fhir-types/r4/types";
 import { FHIR_VERSION } from "@iguhealth/fhir-types/versions";
 import { OperationError, outcomeError } from "@iguhealth/operation-outcomes";
 
 import { FHIRServerCTX } from "../../../../../fhir-api/types.js";
 import {
   SearchParameterResource,
+  getDatePrecision,
   parseValuePrefix,
 } from "../../../../utilities/search/parameters.js";
 import { missingModifier } from "./shared.js";
+
+function getValueRange(value: string): [string, string] {
+  // yyyy-mm-ddThh:mm:ss[Z|(+|-)hh:mm]
+  const formattedDate = dayjs(value, "YYYY-MM-DDThh:mm:ssZ");
+  const precision = getDatePrecision(value as dateTime | date);
+
+  switch (precision) {
+    case "year":
+    case "month":
+    case "day":
+    case "hour":
+    case "minute":
+    case "second": {
+      return [
+        dayjs(formattedDate).startOf(precision).toISOString(),
+        dayjs(formattedDate).endOf(precision).toISOString(),
+      ];
+    }
+  }
+}
 
 export default function dateClauses(
   _ctx: FHIRServerCTX,
@@ -26,28 +48,21 @@ export default function dateClauses(
         ...parameter.value.map(
           (parameterValue): s.r4_date_idx.Whereable | db.SQLFragment => {
             const { prefix, value } = parseValuePrefix(parameterValue);
-            const formattedDate = db.param(
-              dayjs(value, "YYYY-MM-DDThh:mm:ss+zz:zz").toISOString(),
-            );
+            const [startValueRange, endValueRange] = getValueRange(value);
 
             switch (prefix) {
               case "eq":
               case undefined: {
                 return {
-                  start_date: db.sql`${db.self} <= ${formattedDate}`,
-                  end_date: db.sql`${db.self} >= ${formattedDate}`,
+                  start_date: db.sql`${db.self} <= ${db.param(endValueRange)}`,
+                  end_date: db.sql`${db.self} >= ${db.param(startValueRange)}`,
                 };
               }
               case "ne": {
                 return db.sql<s.r4_date_idx.SQL | s.r4b_date_idx.SQL>`
-              ${"start_date"} > ${formattedDate} OR
-              ${"end_date"}   < ${formattedDate}`;
+              ${"start_date"} > ${db.param(endValueRange)} OR
+              ${"end_date"}   < ${db.param(startValueRange)}`;
               }
-
-              // case "gt": {
-              //   return db.sql<s.r4_date_idx.SQL | s.r4b_date_idx.SQL>`
-              //   `;
-              // }
 
               default: {
                 throw new OperationError(
