6b13efd1c45708534a24ed37f76c9cd695ef9a81
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-06-29 16:09:45 -0500

    Documentation Update Postman + Blog (#717)
    * init
    
    * qalt
    
    * blogs
    
    * postman blog

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -83,6 +83,8 @@ const config = {
             "https://github.com/IGUHealth/IGUHealth/tree/main/packages/docs",
         },
         blog: {
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
