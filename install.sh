a8102f48863579faacbf5c81d196d7cdc0a4ad91
confused-moniker <ralph@genfhi.app>
2024-07-13 17:46:19 -0500

    move

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ corepack enable
 yarn install
 yarn workspaces foreach -A -pt --topological-dev run build
 
-
-
 # Setup default .env for server.
 if [ ! -f ./packages/server/.env ]; then
     cp packages/server/.env.defaults ./packages/server/.env 
@@ -16,5 +14,3 @@ if [ ! -f ./packages/admin-app/.env ]; then
     cp packages/admin-app/.env.defaults ./packages/admin-app/.env 
 fi
 
-# Loading Terminologies
-yarn workspace @iguhealth/server cli terminology load --system iso-4217 iso-3166
