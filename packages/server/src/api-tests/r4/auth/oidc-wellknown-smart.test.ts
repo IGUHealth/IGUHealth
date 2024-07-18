c134d1e3e4c8de9f494b5a05b63a91b45103cee0
confused-moniker <ralph@genfhi.app>
2024-07-18 15:38:25 -0500

    mig test

--- packages/server/src/api-tests/r4/auth/oidc-wellknown-smart.test.ts
+++ packages/server/src/api-tests/r4/auth/oidc-wellknown-smart.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@jest/globals";
 
 test("test oidc discovery tenant", async () => {
   const oidcDocument = await fetch(
-    "http://localhost:3000/w/system/oidc/.well-known/smart-configuration",
+    "http://localhost:3000/w/system/api/v1/fhir/r4/.well-known/smart-configuration",
   ).then((res) => res.json());
 
   expect(oidcDocument).toHaveProperty("issuer");
