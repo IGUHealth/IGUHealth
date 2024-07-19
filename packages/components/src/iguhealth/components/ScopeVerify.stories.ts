aa453a717be7d5445d96cd890e3d9ddf0c667640
confused-moniker <ralph@genfhi.app>
2024-07-19 11:37:27 -0500

    init

---
+++ packages/components/src/iguhealth/components/ScopeVerify.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ScopeVerifyForm } from "./ScopeVerify";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "IGUHealth/ScopeVerify",
+  component: ScopeVerifyForm,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof ScopeVerifyForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    title: "IGUHealth",
+    acceptURL: "#",
+    denyURL: "#",
+    scopes: ["openid", "profile", "email"],
+  },
+};
