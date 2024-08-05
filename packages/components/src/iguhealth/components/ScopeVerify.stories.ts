276c779aea6c7d623a38c90adf560fcf8e22b260
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-08-05 16:53:50 -0500

    ℹ Include Info about app on scope (#802)

--- packages/components/src/iguhealth/components/ScopeVerify.stories.ts
+++ packages/components/src/iguhealth/components/ScopeVerify.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import { ClientApplication } from "@iguhealth/fhir-types/lib/generated/r4/types";
+
 import { ScopeVerifyForm } from "./ScopeVerify";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -20,6 +22,47 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
+    client: {
+      name: "Admin App",
+      logoUri:
+        "https://www.gravatar.com/avatar/d23aeb14e1da44674f5e9036abdc9305?s=160&d=retro&r=g",
+    } as ClientApplication,
+    title: "IGUHealth",
+    actionURL: "#",
+    scopes: [
+      "launch/patient",
+      "openid",
+      "fhirUser",
+      "offline_access",
+      "patient/Medication.rs",
+      "patient/AllergyIntolerance.rs",
+      "patient/CarePlan.rs",
+      "patient/CareTeam.rs",
+      "patient/Condition.rs",
+      "patient/Device.rs",
+      "patient/DiagnosticReport.rs",
+      "patient/DocumentReference.rs",
+      "patient/Encounter.rs",
+      "patient/Goal.rs",
+      "patient/Immunization.rs",
+      "patient/Location.rs",
+      "patient/MedicationRequest.rs",
+      "patient/Observation.rs",
+      "patient/Organization.rs",
+      "patient/Patient.rs",
+      "patient/Practitioner.rs",
+      "patient/Procedure.rs",
+      "patient/Provenance.rs",
+      "patient/PractitionerRole.rs",
+    ],
+  },
+};
+
+export const NoImage: Story = {
+  args: {
+    client: {
+      name: "Admin App",
+    } as ClientApplication,
     title: "IGUHealth",
     actionURL: "#",
     scopes: [
