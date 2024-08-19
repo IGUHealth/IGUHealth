0d09f81fb34c59c48c079a14b80e88bd06dd7250
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-08-19 13:57:58 -0500

    ⛓️ Alt URL concat (#822)

--- packages/server/src/fhir-api/constants.ts
+++ packages/server/src/fhir-api/constants.ts
@@ -1,19 +1,25 @@
+import path from "node:path";
+
 import { uri } from "@iguhealth/fhir-types/r4/types";
 import { FHIR_VERSION, R4, R4B } from "@iguhealth/fhir-types/versions";
 import { TenantId } from "@iguhealth/jwt";
 
-export function createTenantURL(tenant: TenantId) {
-  return `${process.env.API_URL}/w/${tenant}`;
+export function createTenantURL(tenant: TenantId): string {
+  const base = new URL(process.env.API_URL);
+  return new URL(path.join(base.pathname, "w", tenant), base.origin).href;
 }
 
 export function createFHIRURL(
   fhirVersion: FHIR_VERSION,
   tenant: TenantId,
   end: string,
-): uri | undefined {
+): uri {
   const fhirSlug =
     fhirVersion === R4 ? "r4" : fhirVersion === R4B ? "r4b" : fhirVersion;
-  return `${createTenantURL(tenant)}/api/v1/fhir/${fhirSlug}/${end}` as
-    | uri
-    | undefined;
+  const tenantURL = new URL(createTenantURL(tenant));
+
+  return new URL(
+    path.join(tenantURL.pathname, "api/v1/fhir", fhirSlug, end),
+    tenantURL.origin,
+  ).href as uri;
 }
