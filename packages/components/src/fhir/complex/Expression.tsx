c7b09695cf884ce3f5b525028c70f29a61342211
confused-moniker <ralph@genfhi.app>
2024-09-05 15:27:21 -0500

    wrap container

--- packages/components/src/fhir/complex/Expression.tsx
+++ packages/components/src/fhir/complex/Expression.tsx
@@ -1,35 +1,41 @@
+import { minimalSetup } from "codemirror";
 import React from "react";
 
 import { Expression, code } from "@iguhealth/fhir-types/r4/types";
 import fp from "@iguhealth/lang-fp";
 
-import { CodeMirror } from "../../base";
+import { CodeMirror, InputContainer } from "../../base";
 import { EditableProps } from "../types";
 
 export type FHIRExpressionEditableProps = EditableProps<Expression>;
 
-const extensions = [fp()];
+const extensions = [minimalSetup, fp()];
 
-export function FHIRExpressionEditable(props: FHIRExpressionEditableProps) {
+export function FHIRExpressionEditable({
+  value,
+  label,
+  issue,
+  onChange,
+}: FHIRExpressionEditableProps) {
   return (
-    <div className="flex flex-1 border overflow-auto">
+    <InputContainer label={label} issues={issue ? [issue] : []}>
       <CodeMirror
         extensions={extensions}
-        value={props.value?.expression}
+        value={value?.expression}
         theme={{
           "&": {
             height: "100%",
             width: "100%",
           },
         }}
-        onChange={(value) => {
-          props.onChange?.call(undefined, {
-            ...props.value,
+        onChange={(expression) => {
+          onChange?.call(undefined, {
+            ...value,
             language: "text/fhirpath" as code,
-            expression: value,
+            expression: expression,
           });
         }}
       />
-    </div>
+    </InputContainer>
   );
 }
