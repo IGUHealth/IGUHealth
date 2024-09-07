8cec283d26f707bfab513cf556732baac6435e3a
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-09-07 18:11:04 -0500

    SQL Constraint (#868)
    * SQL constraint global unique email
    
    * ver
    
    * mod

---
+++ packages/server/src/fhir-storage/providers/postgres/migrations/db-migrate/sqls/20240907175239-unique-constraint-down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE users
+DROP CONSTRAINT unique_email;
+
+DROP INDEX owner_unique_idx;
---
+++ packages/server/src/fhir-storage/providers/postgres/migrations/db-migrate/sqls/20240907175239-unique-constraint-up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE users ADD CONSTRAINT unique_email UNIQUE NULLS NOT DISTINCT (tenant, email, method, fhir_provider_id);
+
+CREATE UNIQUE INDEX owner_unique_idx ON users (email)
+WHERE
+    role = 'owner'
+    AND email_verified = true;
