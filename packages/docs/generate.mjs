f00fe50a9d3130bc9f728bb3ae0549d50b2c74c4
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-09-25 22:35:13 -0500

    Q Update generated docs (#904)
    * update npm
    
    * update fhir docs

--- packages/docs/generate.mjs
+++ packages/docs/generate.mjs
@@ -8,8 +8,9 @@ import { R4, R4B } from "@iguhealth/fhir-types/versions";
 const r4Artifacts = ["StructureDefinition", "SearchParameter"]
   .map((resourceType) =>
     loadArtifacts({
+      loadDevelopmentPackages: true,
       resourceType: resourceType,
-      packageLocation: path.join(fileURLToPath(import.meta.url), "../"),
+      packageLocation: path.join(fileURLToPath(import.meta.url), "."),
       silence: false,
       fhirVersion: R4,
     }),
@@ -19,6 +20,7 @@ const r4Artifacts = ["StructureDefinition", "SearchParameter"]
 const r4bArtifacts = ["StructureDefinition", "SearchParameter"]
   .map((resourceType) =>
     loadArtifacts({
+      loadDevelopmentPackages: true,
       resourceType: resourceType,
       packageLocation: path.join(fileURLToPath(import.meta.url), "../"),
       silence: false,
