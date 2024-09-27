9f1f657fa20120da689c20e0dd8afc3f6b9f9434
confused-moniker <ralph@genfhi.app>
2024-09-27 17:43:00 -0500

    mod

--- packages/server/src/authN/oidc/middleware/inject-idps.ts
+++ packages/server/src/authN/oidc/middleware/inject-idps.ts
@@ -1,38 +1,8 @@
 import * as Koa from "koa";
 
-import { ClientApplication, id } from "@iguhealth/fhir-types/r4/types";
 import { R4 } from "@iguhealth/fhir-types/versions";
 
 import { KoaExtensions, asRoot } from "../../../fhir-api/types.js";
-import getHardCodedClients from "../hardcodedClients/index.js";
-
-export async function findClient(
-  ctx: Koa.ParameterizedContext<
-    KoaExtensions.IGUHealth,
-    KoaExtensions.KoaIGUHealthContext
-  >,
-  clientId: string,
-): Promise<ClientApplication | undefined> {
-  switch (true) {
-    case getHardCodedClients().find((client) => client.id === clientId) !==
-      undefined: {
-      return getHardCodedClients().find((client) => client.id === clientId);
-    }
-    default: {
-      try {
-        const clientApp = await ctx.state.iguhealth.client.read(
-          await asRoot(ctx.state.iguhealth),
-          R4,
-          "ClientApplication",
-          clientId as id,
-        );
-        return clientApp;
-      } catch (err) {
-        return undefined;
-      }
-    }
-  }
-}
 
 /**
  * Creates koa middleware that injects the current ClientApplication under ctx.state.oidc.client.
