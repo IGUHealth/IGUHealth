cfe3b141385c6d6d316249cc2a9515dec3c4fa07
confused-moniker <ralph@genfhi.app>
2024-11-12 14:41:28 -0600

    mod

--- packages/fhir-validation/src/profile/index.ts
+++ packages/fhir-validation/src/profile/index.ts
@@ -23,6 +23,40 @@ import { OperationError, outcomeFatal } from "@iguhealth/operation-outcomes";
 import { ElementLoc, ValidationCTX } from "../types.js";
 import validateAllSlicesAtLocation from "./slicing/index.js";
 
+/**
+ * Because profile is merely a constraint on the base we only need to validate select pieces.
+ *
+ * [ALLOWED]:
+ * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * Restricting the cardinality of the element; e.g. the base might allow 0..*, and a particular application might support 1..2
+ * Ruling out use of an element by setting its maximum cardinality to 0
+ * Restricting the contents of an element to a single fixed value
+ * Making additional constraints on the content of nested elements within the resource (expressed as FHIRPath statements)
+ * Restricting the types for an element that allows multiple types
+ * Requiring a typed element or the target of a resource reference to conform to another structure profile (declared in the same profile, or elsewhere)
+ * Specifying a binding to a different terminology value set (see below)
+ * Providing refined definitions, comments/usage notes and examples for the elements defined in a Resource to reflect the usage of the element within the context of the Profile
+ * Providing more specific or additional mappings (e.g. to HL7 V2 icon or HL7 v3 icon) for the resource when used in a particular context
+ * Declaring that one or more elements in the structure must be 'supported' (see below)
+ * Restricting / clarifying a mapping by providing a new mapping with the same identity, which means that the new mapping replaces a mapping with the same identity in the element being profiled
+ * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * [DISSALLOWED]:
+ * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * Profiles cannot break the rules established in the base specification (e.g. cardinality as described above)
+ * Profiles cannot specify default values or meanings for elements defined in the base specification (note that datatypes and resources do not
+ *   define default values at all, but default values may be defined for logical models
+ * Profiles cannot change the name of elements defined in the base specification, or add new elements
+ * It must be safe to process a resource without knowing the profile
+ * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * @param ctx Validation CTX
+ * @param profile Profile SD
+ * @param elementLoc Current ElementDefinition validating
+ * @param root Root value to be validated
+ * @param path Path inside of value to validate
+ * @param type
+ * @returns
+ */
 async function validateProfileElement(
   ctx: ValidationCTX,
   profile: Resource<FHIR_VERSION, "StructureDefinition">,
