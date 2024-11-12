bce0fd83dfd504bfcf61905b433d6de8478b2f2c
confused-moniker <ralph@genfhi.app>
2024-11-12 14:34:14 -0600

    mod

--- packages/fhir-validation/src/profile/index.ts
+++ packages/fhir-validation/src/profile/index.ts
@@ -1,64 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { eleIndexToChildIndices } from "@iguhealth/codegen/traversal/structure-definition";
-import { Loc, toJSONPointer, typedPointer } from "@iguhealth/fhir-pointer";
+import {
+  Loc,
+  descend,
+  pointer,
+  toJSONPointer,
+  typedPointer,
+} from "@iguhealth/fhir-pointer";
 import {
   OperationOutcome,
-  StructureDefinition,
+  OperationOutcomeIssue,
   canonical,
+  id,
+  uri,
 } from "@iguhealth/fhir-types/r4/types";
 import * as r4b from "@iguhealth/fhir-types/r4b/types";
 import { FHIR_VERSION, Resource } from "@iguhealth/fhir-types/versions";
 import { IMetaValue } from "@iguhealth/meta-value/interface";
 import { OperationError, outcomeFatal } from "@iguhealth/operation-outcomes";
 
-import { ValidationCTX } from "../types.js";
+import { ElementLoc, ValidationCTX } from "../types.js";
 import validateAllSlicesAtLocation from "./slicing/index.js";
 
-function getNonSliceChildren(
-  profile: Resource<FHIR_VERSION, "StructureDefinition">,
-  differentialIndex: number,
-): number[] {
-  const differential = profile.differential?.element ?? [];
-  const childrenIndices = eleIndexToChildIndices(
-    differential,
-    differentialIndex,
-  );
-
-  return childrenIndices.filter((childIndex) => {
-    const element = differential[childIndex];
-    // Filter out children with slices which are validated seperately.
-    return element.slicing === undefined && element.sliceName === undefined;
-  });
-}
-
-async function validateDifferential(
+async function validateProfileElement(
   ctx: ValidationCTX,
-  profile: StructureDefinition | r4b.StructureDefinition,
-  elementIndex: number,
+  profile: Resource<FHIR_VERSION, "StructureDefinition">,
+  elementLoc: ElementLoc,
   root: object,
   path: Loc<any, any, any>,
-) {
-  let issues = await validateAllSlicesAtLocation(
-    ctx,
-    profile,
-    elementIndex,
-    root,
-    path,
-  );
-
-  const childrenIndices = getNonSliceChildren(profile, elementIndex);
-  issues = issues.concat(
-    (
-      await Promise.all(
-        childrenIndices.map((childIndex) =>
-          validateDifferential(ctx, profile, childIndex, root, path),
-        ),
-      )
-    ).flat(),
-  );
-
-  return issues;
+  type: uri,
+): Promise<OperationOutcomeIssue[]> {
+  // For Profiling validate changes in the cardinality and slices.
+  // No need to validate the nested type only at the top level (because structural would have already confirmed this).
+  return [];
 }
 
 /**
@@ -71,15 +46,14 @@ async function validateDifferential(
 export default async function (
   ctx: ValidationCTX,
   profileURL: canonical,
-  value: IMetaValue<unknown>,
+  root: IMetaValue<unknown>,
   path: Loc<any, any, any> = typedPointer<any, any>(),
-): Promise<OperationOutcome> {
+): Promise<OperationOutcomeIssue[]> {
   const profile = await ctx.resolveCanonical(
     ctx.fhirVersion,
     "StructureDefinition",
     profileURL,
   );
-
   if (!profile) {
     throw new OperationError(
       outcomeFatal(
@@ -89,6 +63,32 @@ export default async function (
       ),
     );
   }
+  if (profile.derivation !== "constraint") {
+    throw new OperationError(
+      outcomeFatal(
+        "invalid",
+        `Profile must be a constraint profile, got ${profile.derivation}`,
+        [toJSONPointer(path)],
+      ),
+    );
+  }
 
-  throw new Error();
+  const issues: OperationOutcomeIssue[] = [];
+
+  const startingLoc = descend(
+    descend(
+      descend(pointer("StructureDefinition", profile.id as id), "snapshot"),
+      "element",
+    ),
+    0,
+  );
+
+  return validateProfileElement(
+    ctx,
+    profile,
+    startingLoc as unknown as ElementLoc,
+    root,
+    path,
+    profile.type,
+  );
 }
