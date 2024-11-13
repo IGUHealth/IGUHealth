28e505f66307dac8d5d33334e5c772b94a60960e
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-11-13 17:00:07 -0600

    🔭 Profile Full Validation Entry (#963)
    * init
    
    * move
    
    * comment clarification
    
    * rm fixme

--- packages/fhir-validation/src/profile/index.test.ts
+++ packages/fhir-validation/src/profile/index.test.ts
@@ -1,3 +1,274 @@
 import { expect, test } from "@jest/globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-test("Test BP profile", async () => {});
+import { loadArtifacts } from "@iguhealth/artifacts";
+import {
+  Observation,
+  StructureDefinition,
+  canonical,
+  uri,
+} from "@iguhealth/fhir-types/r4/types";
+import {
+  AllResourceTypes,
+  FHIR_VERSION,
+  R4,
+  Resource,
+  ResourceType,
+} from "@iguhealth/fhir-types/versions";
+
+import { ValidationCTX } from "../types";
+import { validateProfile } from "./index.js";
+
+function createMemoryDatabase(
+  resourceTypes: ResourceType<R4>[],
+): Record<ResourceType<R4>, Resource<FHIR_VERSION, AllResourceTypes>[]> {
+  const data: Record<
+    ResourceType<R4>,
+    Resource<FHIR_VERSION, AllResourceTypes>[]
+  > = {} as Record<
+    ResourceType<R4>,
+    Resource<FHIR_VERSION, AllResourceTypes>[]
+  >;
+  for (const resourceType of resourceTypes) {
+    const resources = loadArtifacts({
+      fhirVersion: R4,
+      loadDevelopmentPackages: true,
+      resourceType: resourceType,
+      packageLocation: path.join(fileURLToPath(import.meta.url), "../.."),
+      silence: true,
+      onlyPackages: [
+        "hl7.fhir.us.core",
+        "@iguhealth/hl7.fhir.r4.core",
+        "@iguhealth/hl7.fhir.r4.test-data",
+      ],
+    });
+    data[resourceType] = resources;
+  }
+
+  return data;
+}
+
+const memDatabase = createMemoryDatabase([
+  "StructureDefinition",
+] as ResourceType<R4>[]);
+
+const CTX: ValidationCTX = {
+  fhirVersion: R4,
+  resolveTypeToCanonical: async (
+    version: FHIR_VERSION,
+    type: uri,
+  ): Promise<canonical> => {
+    return `http://hl7.org/fhir/StructureDefinition/${type}` as canonical;
+  },
+  resolveCanonical: async <
+    Version extends FHIR_VERSION,
+    Type extends ResourceType<Version>,
+  >(
+    version: Version,
+    t: Type,
+    url: canonical,
+  ): Promise<Resource<Version, Type>> => {
+    // @ts-ignore
+    const sd: Resource<Version, Type> = memDatabase[t].find(
+      (sd: unknown) => (sd as StructureDefinition).url === url,
+    ) as Resource<Version, Type>;
+    if (!sd) throw new Error(`Couldn't find sd with url '${url}'`);
+    return sd as Resource<Version, Type>;
+  },
+};
+
+const bloodPressureObservation: Observation = {
+  id: "blood-pressure-observation",
+  resourceType: "Observation",
+  status: "final",
+  category: [
+    {
+      coding: [
+        {
+          system: "http://terminology.hl7.org/CodeSystem/observation-category",
+          code: "vital-signs",
+          display: "Vital Signs",
+        },
+      ],
+      text: "Vital Signs",
+    },
+  ],
+  code: {
+    coding: [
+      {
+        system: "http://loinc.org",
+        code: "85354-9",
+        display: "Blood pressure panel with all children optional",
+      },
+    ],
+    text: "Blood pressure systolic and diastolic",
+  },
+  subject: {
+    reference: "Patient/example",
+    display: "Amy Shaw",
+  },
+  encounter: {
+    display: "GP Visit",
+  },
+  effectiveDateTime: "1999-07-02",
+  component: [
+    {
+      code: {
+        coding: [
+          {
+            system: "http://loinc.org",
+            code: "8480-6",
+            display: "Systolic blood pressure",
+          },
+        ],
+        text: "Systolic blood pressure",
+      },
+      valueQuantity: {
+        value: 109,
+        unit: "mmHg",
+        system: "http://unitsofmeasure.org",
+        code: "mm[Hg]",
+      },
+    },
+    {
+      code: {
+        coding: [
+          {
+            system: "http://loinc.org",
+            code: "8462-4",
+            display: "Diastolic blood pressure",
+          },
+        ],
+        text: "Diastolic blood pressure",
+      },
+      valueQuantity: {
+        value: 44,
+        unit: "mmHg",
+        system: "http://unitsofmeasure.org",
+        code: "mm[Hg]",
+      },
+    },
+  ],
+} as Observation;
+
+const bloodProfile: StructureDefinition = memDatabase[
+  "StructureDefinition"
+].find(
+  (p) =>
+    (p as StructureDefinition).url ===
+    "http://hl7.org/fhir/us/core/StructureDefinition/us-core-blood-pressure",
+) as StructureDefinition;
+test("Test BP profile", async () => {
+  await validateProfile(CTX, bloodProfile, bloodPressureObservation);
+  expect(
+    validateProfile(CTX, bloodProfile, bloodPressureObservation),
+  ).resolves.toEqual([]);
+
+  expect(
+    validateProfile(CTX, bloodProfile, {
+      ...bloodPressureObservation,
+      component: [],
+    }),
+  ).resolves.toEqual([
+    {
+      code: "structure",
+      diagnostics:
+        "Slice 'systolic' does not have the minimum number of values.",
+      expression: undefined,
+      severity: "error",
+    },
+    {
+      code: "structure",
+      diagnostics:
+        "Slice 'diastolic' does not have the minimum number of values.",
+      expression: undefined,
+      severity: "error",
+    },
+  ]);
+});
+
+test("Invalid type constraint systolic.value", async () => {
+  await validateProfile(CTX, bloodProfile, {
+    ...bloodPressureObservation,
+    component: [
+      {
+        code: {
+          coding: [
+            {
+              system: "http://loinc.org",
+              code: "8480-6",
+              display: "Systolic blood pressure",
+            },
+          ],
+          text: "Systolic blood pressure",
+        },
+        valueInteger: 5,
+      },
+      {
+        code: {
+          coding: [
+            {
+              system: "http://loinc.org",
+              code: "8462-4",
+              display: "Diastolic blood pressure",
+            },
+          ],
+          text: "Diastolic blood pressure",
+        },
+        valueQuantity: {
+          value: 44,
+          unit: "mmHg",
+          system: "http://unitsofmeasure.org",
+          code: "mm[Hg]",
+        },
+      },
+    ],
+  });
+  expect(
+    validateProfile(CTX, bloodProfile, {
+      ...bloodPressureObservation,
+      component: [
+        {
+          code: {
+            coding: [
+              {
+                system: "http://loinc.org",
+                code: "8480-6",
+                display: "Systolic blood pressure",
+              },
+            ],
+            text: "Systolic blood pressure",
+          },
+          valueInteger: 5,
+        },
+        {
+          code: {
+            coding: [
+              {
+                system: "http://loinc.org",
+                code: "8462-4",
+                display: "Diastolic blood pressure",
+              },
+            ],
+            text: "Diastolic blood pressure",
+          },
+          valueQuantity: {
+            value: 44,
+            unit: "mmHg",
+            system: "http://unitsofmeasure.org",
+            code: "mm[Hg]",
+          },
+        },
+      ],
+    }),
+  ).resolves.toEqual([
+    {
+      severity: "error",
+      code: "structure",
+      diagnostics:
+        "Additional fields found at path '/component/0': 'valueInteger'",
+      expression: ["/component/0"],
+    },
+  ]);
+});
--- packages/fhir-validation/src/profile/index.ts
+++ packages/fhir-validation/src/profile/index.ts
@@ -1,131 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { eleIndexToChildIndices } from "@iguhealth/codegen/traversal/structure-definition";
 import {
   Loc,
   descend,
-  get,
   pointer,
   toJSONPointer,
   typedPointer,
 } from "@iguhealth/fhir-pointer";
 import {
-  ElementDefinition,
   OperationOutcomeIssue,
-  StructureDefinition,
   canonical,
   id,
-  uri,
 } from "@iguhealth/fhir-types/r4/types";
 import { FHIR_VERSION, Resource } from "@iguhealth/fhir-types/versions";
-import { IMetaValue } from "@iguhealth/meta-value/interface";
 import { OperationError, outcomeFatal } from "@iguhealth/operation-outcomes";
 
 import { ElementLoc, ValidationCTX } from "../types.js";
-import { ascendElementLoc } from "../utilities.js";
-import validateAllSlicesAtLocation from "./slicing/index.js";
+import { validateProfileElement } from "./element.js";
 
-/**
- * For descendants we want to ignore those slices which are validated seperately.
- * This is done by filtering off sliceName and slicing property.
- *
- * @param elements list of ElementDefinitions to filter
- * @param indices The indices to filter out of the list if they are a part of a lice.
- * @returns Indices that are not a part of a slice.
- */
-function ignoreSliceElements(
-  elements: ElementDefinition[],
-  indices: number[],
-): number[] {
-  return indices.filter((index) => {
-    const element = elements[index];
-    return element.sliceName === undefined && element.slicing === undefined;
-  });
-}
-
-/**
- * Because profile is merely a constraint on the base we only need to validate select pieces.
- *
- * [ALLOWED]:
- * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- * Restricting the cardinality of the element; e.g. the base might allow 0..*, and a particular application might support 1..2
- * Ruling out use of an element by setting its maximum cardinality to 0
- * Restricting the contents of an element to a single fixed value
- * Making additional constraints on the content of nested elements within the resource (expressed as FHIRPath statements)
- * Restricting the types for an element that allows multiple types
- * Requiring a typed element or the target of a resource reference to conform to another structure profile (declared in the same profile, or elsewhere)
- * Specifying a binding to a different terminology value set (see below)
- * Providing refined definitions, comments/usage notes and examples for the elements defined in a Resource to reflect the usage of the element within the context of the Profile
- * Providing more specific or additional mappings (e.g. to HL7 V2 icon or HL7 v3 icon) for the resource when used in a particular context
- * Declaring that one or more elements in the structure must be 'supported' (see below)
- * Restricting / clarifying a mapping by providing a new mapping with the same identity, which means that the new mapping replaces a mapping with the same identity in the element being profiled
- * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- *
- * [DISSALLOWED]:
- * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- * Profiles cannot break the rules established in the base specification (e.g. cardinality as described above)
- * Profiles cannot specify default values or meanings for elements defined in the base specification (note that datatypes and resources do not
- *   define default values at all, but default values may be defined for logical models
- * Profiles cannot change the name of elements defined in the base specification, or add new elements
- * It must be safe to process a resource without knowing the profile
- * --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- * @param ctx Validation CTX
- * @param profile Profile SD
- * @param elementLoc Current ElementDefinition validating
- * @param root Root value to be validated
- * @param path Path inside of value to validate
- * @param type
- * @returns
- */
-async function validateProfileElement(
+export async function validateProfile(
   ctx: ValidationCTX,
   profile: Resource<FHIR_VERSION, "StructureDefinition">,
-  elementLoc: ElementLoc,
   root: object,
-  path: Loc<any, any, any>,
-  type: uri,
-): Promise<OperationOutcomeIssue[]> {
-  const { parent: elementsLoc, field: elementIndex } =
-    ascendElementLoc(elementLoc);
-
-  const elements = get(elementsLoc, profile as StructureDefinition);
-  const children = ignoreSliceElements(
-    elements,
-    eleIndexToChildIndices(elements, elementIndex as number),
-  );
-
-  await validateAllSlicesAtLocation(ctx, profile, elementLoc, root, path);
-
-  return [];
-}
-
-/**
- *
- * @param ctx_ Validation CTX
- * @param profileURL Profile canonical to validate
- * @param value the value to check
- * @param path The current path of the value.
- */
-export default async function (
-  ctx: ValidationCTX,
-  profileURL: canonical,
-  root: IMetaValue<unknown>,
   path: Loc<any, any, any> = typedPointer<any, any>(),
 ): Promise<OperationOutcomeIssue[]> {
-  const profile = await ctx.resolveCanonical(
-    ctx.fhirVersion,
-    "StructureDefinition",
-    profileURL,
-  );
-  if (!profile) {
-    throw new OperationError(
-      outcomeFatal(
-        "not-found",
-        `Unable to resolve canonical for profile '${profileURL}'`,
-        [toJSONPointer(path)],
-      ),
-    );
-  }
   if (profile.derivation !== "constraint") {
     throw new OperationError(
       outcomeFatal(
@@ -135,8 +33,6 @@ export default async function (
       ),
     );
   }
-
-  const issues: OperationOutcomeIssue[] = [];
 
   const startingLoc = descend(
     descend(
@@ -154,4 +50,36 @@ export default async function (
     path,
     profile.type,
   );
+}
+
+/**
+ *
+ * @param ctx_ Validation CTX
+ * @param profileURL Profile canonical to validate
+ * @param value the value to check
+ * @param path The current path of the value.
+ */
+export default async function validateProfileCanonical(
+  ctx: ValidationCTX,
+  profileURL: canonical,
+  root: object,
+  path: Loc<any, any, any> = typedPointer<any, any>(),
+): Promise<OperationOutcomeIssue[]> {
+  const profile = await ctx.resolveCanonical(
+    ctx.fhirVersion,
+    "StructureDefinition",
+    profileURL,
+  );
+
+  if (!profile) {
+    throw new OperationError(
+      outcomeFatal(
+        "not-found",
+        `Unable to resolve canonical for profile '${profileURL}'`,
+        [toJSONPointer(path)],
+      ),
+    );
+  }
+
+  return validateProfile(ctx, profile, root, path);
 }
