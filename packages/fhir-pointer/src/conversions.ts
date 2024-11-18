1da5f5f801dcd4496b033d4c2d197ae29d2aaf18
confused-moniker <ralph@genfhi.app>
2024-11-18 16:17:35 -0600

    alt

--- packages/fhir-pointer/src/conversions.ts
+++ packages/fhir-pointer/src/conversions.ts
@@ -1,6 +1,13 @@
-import r4MetaData from "@iguhealth/meta-value/v2/meta/r4";
-import r4bMetaData from "@iguhealth/meta-value/v2/meta/r4b";
+import {
+  AllDataTypes,
+  Data,
+  FHIR_VERSION,
+} from "@iguhealth/fhir-types/versions";
 
+// import r4MetaData from "@iguhealth/meta-value/v2/meta/r4";
+// import r4bMetaData from "@iguhealth/meta-value/v2/meta/r4b";
+
+import { pathMeta } from "./index.js";
 import { Loc, Parent } from "./types.js";
 import { unescapeField } from "./utilities.js";
 
@@ -10,8 +17,16 @@ export function toJSONPointer<T, R, P extends Parent<T>>(loc: Loc<T, R, P>) {
   return loc.substring(indexOfLastSlash, loc.length);
 }
 
-export function toFHIRPath<T, R, P extends Parent<T>>(loc: Loc<T, R, P>) {
+export function toFHIRPath<
+  T extends Data<FHIR_VERSION, AllDataTypes>,
+  R,
+  P extends Parent<T>,
+>(loc: Loc<T, R, P>) {
   const indexOfLastSlash = loc.indexOf("/");
+
+  const { version, type } = pathMeta(loc);
+  console.log(version, type);
+
   if (indexOfLastSlash === -1) return "$this";
   const pieces = loc.substring(indexOfLastSlash + 1).split("/");
   let fp = "$this";
