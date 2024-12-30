0082268e5c7a31b549a0618d13737a4d5b35b9ec
confused-moniker <ralph@genfhi.app>
2024-12-30 17:19:05 -0600

    mod

--- packages/client/src/middleware/index.ts
+++ packages/client/src/middleware/index.ts
@@ -6,9 +6,10 @@ export type MiddlewareAsyncChain<
   Request = FHIRRequest,
   Response = FHIRResponse,
 > = (
-  ctx: { state: State; ctx: CTX; request: Request },
-  next: MiddlewareAsync<State, CTX>,
+  ctx: { key?: string; state: State; ctx: CTX; request: Request },
+  next: MiddlewareAsync<State, CTX, Request, Response>,
 ) => Promise<{
+  key?: string;
   ctx: CTX;
   state: State;
   response?: Response;
@@ -20,34 +21,42 @@ export type MiddlewareAsync<
   CTX,
   Request = FHIRRequest,
   Response = FHIRResponse,
-> = (ctx: { state: State; ctx: CTX; request: Request }) => Promise<{
+> = (ctx: {
+  key?: string;
+  state: State;
+  ctx: CTX;
+  request: Request;
+}) => Promise<{
+  key?: string;
   ctx: CTX;
   state: State;
   response?: Response;
   request: Request;
 }>;
 
-export function createMiddlewareAsync<State, CTX>(
-  middleware: MiddlewareAsyncChain<State, CTX>[],
+export function createMiddlewareAsync<
+  State,
+  CTX,
+  Request = FHIRRequest,
+  Response = FHIRResponse,
+>(
+  middleware: MiddlewareAsyncChain<State, CTX, Request, Response>[],
   options: { logging?: boolean } = { logging: false },
-): MiddlewareAsync<State, CTX> {
+): MiddlewareAsync<State, CTX, Request, Response> {
   return async (context) => {
     const [first, ...rest] = middleware;
-    context.request.key =
-      context.request.key ?? Math.ceil(Math.random() * 10000).toString();
+    context.key = context.key ?? Math.ceil(Math.random() * 10000).toString();
     if (rest[0]) {
-      if (options.logging) console.time(`${context.request.key}:${first.name}`);
+      if (options.logging) console.time(`${context.key}:${first.name}`);
       const response = await first(context, (req) => {
         return createMiddlewareAsync(rest, options)(req);
       });
-      if (options.logging)
-        console.timeEnd(`${context.request.key}:${first.name}`);
+      if (options.logging) console.timeEnd(`${context.key}:${first.name}`);
       return response;
     } else {
-      if (options.logging) console.time(`${context.request.key}:${first.name}`);
+      if (options.logging) console.time(`${context.key}:${first.name}`);
       const response = await first(context, async (v) => v);
-      if (options.logging)
-        console.timeEnd(`${context.request.key}:${first.name}`);
+      if (options.logging) console.timeEnd(`${context.key}:${first.name}`);
       return response;
     }
   };
