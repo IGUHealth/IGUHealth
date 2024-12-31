5d49c0eca4f743c9e15af7545f8422e34fc70056
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2024-12-30 18:19:38 -0600

    Middleware type alterations (#1017)
    * ver
    
    * mod
    
    * mod
    
    * mod

--- packages/client/src/middleware/index.ts
+++ packages/client/src/middleware/index.ts
@@ -1,47 +1,62 @@
 import { FHIRRequest, FHIRResponse } from "../types/index.js";
 
-export type MiddlewareAsyncChain<State, CTX> = (
-  ctx: { state: State; ctx: CTX; request: FHIRRequest },
-  next: MiddlewareAsync<State, CTX>,
+export type MiddlewareAsyncChain<
+  State,
+  CTX,
+  Request = FHIRRequest,
+  Response = FHIRResponse,
+> = (
+  ctx: { key?: string; state: State; ctx: CTX; request: Request },
+  next: MiddlewareAsync<State, CTX, Request, Response>,
 ) => Promise<{
+  key?: string;
   ctx: CTX;
   state: State;
-  response?: FHIRResponse;
-  request: FHIRRequest;
+  response?: Response;
+  request: Request;
 }>;
 
-export type MiddlewareAsync<State, CTX> = (ctx: {
+export type MiddlewareAsync<
+  State,
+  CTX,
+  Request = FHIRRequest,
+  Response = FHIRResponse,
+> = (ctx: {
+  key?: string;
   state: State;
   ctx: CTX;
-  request: FHIRRequest;
+  request: Request;
 }) => Promise<{
+  key?: string;
   ctx: CTX;
   state: State;
-  response?: FHIRResponse;
-  request: FHIRRequest;
+  response?: Response;
+  request: Request;
 }>;
 
-export function createMiddlewareAsync<State, CTX>(
-  middleware: MiddlewareAsyncChain<State, CTX>[],
+export function createMiddlewareAsync<
+  State,
+  CTX,
+  Request = FHIRRequest,
+  Response = FHIRResponse,
+>(
+  middleware: MiddlewareAsyncChain<State, CTX, Request, Response>[],
   options: { logging?: boolean } = { logging: false },
-): MiddlewareAsync<State, CTX> {
+): MiddlewareAsync<State, CTX, Request, Response> {
   return async (context) => {
     const [first, ...rest] = middleware;
-    context.request.key =
-      context.request.key ?? Math.ceil(Math.random() * 10000).toString();
+    context.key = context.key ?? Math.ceil(Math.random() * 10000).toString();
     if (rest[0]) {
-      if (options.logging) console.time(`${context.request.key}:${first.name}`);
+      if (options.logging) console.time(`${context.key}:${first.name}`);
       const response = await first(context, (req) => {
         return createMiddlewareAsync(rest, options)(req);
       });
-      if (options.logging)
-        console.timeEnd(`${context.request.key}:${first.name}`);
+      if (options.logging) console.timeEnd(`${context.key}:${first.name}`);
       return response;
     } else {
-      if (options.logging) console.time(`${context.request.key}:${first.name}`);
+      if (options.logging) console.time(`${context.key}:${first.name}`);
       const response = await first(context, async (v) => v);
-      if (options.logging)
-        console.timeEnd(`${context.request.key}:${first.name}`);
+      if (options.logging) console.timeEnd(`${context.key}:${first.name}`);
       return response;
     }
   };
