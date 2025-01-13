01796ee6c561f4b20abc1cb13ab0fc1819f659e6
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2025-01-12 19:47:25 -0600

    RM FK Tenant on search tables (#1031)
    * mod
    
    * ver

---
+++ packages/server/src/storage/schemas/migrations/db-migrate/sqls/20250113012938-rm-search-fk-tenant-up.sql
@@ -0,0 +1,49 @@
+/* R4 */
+ALTER TABLE r4_date_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4_number_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4_quantity_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4_reference_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4_sp1_idx
+DROP CONSTRAINT sp1_fk_tenant;
+
+ALTER TABLE r4_string_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4_token_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4_uri_idx
+DROP CONSTRAINT fk_tenant;
+
+/* R4B */
+ALTER TABLE r4b_date_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4b_number_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4b_quantity_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4b_reference_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4b_sp1_idx
+DROP CONSTRAINT sp1_fk_tenant;
+
+ALTER TABLE r4b_string_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4b_token_idx
+DROP CONSTRAINT fk_tenant;
+
+ALTER TABLE r4b_uri_idx
+DROP CONSTRAINT fk_tenant;
