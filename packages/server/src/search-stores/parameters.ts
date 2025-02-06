875d4578a1a2455280a8349036704c2368fd8ea8
confused-moniker <ralph@genfhi.app>
2025-02-06 17:25:35 -0600

    mod

--- packages/server/src/search-stores/parameters.ts
+++ packages/server/src/search-stores/parameters.ts
@@ -278,6 +278,7 @@ export async function parametersWithMetaAssociated(
         type: "resource",
         searchParameter: searchParameter,
       };
+
       return associateChainedParameters(param, resolveSearchParameter);
     }),
   );
