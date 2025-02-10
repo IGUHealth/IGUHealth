6923e186c2abaad78f0c0ced955699cec6656daa
confused-moniker <ralph@genfhi.app>
2025-02-10 13:05:52 -0600

    include check

--- packages/search-parameters/src/api/generate.ts
+++ packages/search-parameters/src/api/generate.ts
@@ -26,6 +26,16 @@ export async function createHash<Version extends FHIR_VERSION>(
 
   for (const parameter of parameters) {
     for (const base of parameter.base) {
+      // Confirm doesn't exist already.
+      if (
+        hash[fhirVersion][base as ResourceType<typeof fhirVersion>]?.[
+          parameter.code
+        ]
+      ) {
+        throw new Error(
+          `Duplicate search parameter '${parameter.code}' for '${base}' in '${fhirVersion}'`,
+        );
+      }
       hash[fhirVersion][base as ResourceType<typeof fhirVersion>] = {
         ...hash[fhirVersion][base as ResourceType<typeof fhirVersion>],
         [parameter.code]: parameter.url,
