3a9b4b652c1658f4b5d57542e9c21fd8325c75c1
confused-moniker <ralph@genfhi.app>
2025-02-13 16:42:28 -0600

    ver

--- artifacts/r4b/hl7-core/fix.mjs
+++ artifacts/r4b/hl7-core/fix.mjs
@@ -11,18 +11,33 @@ const content = JSON.parse(fs.readFileSync(PATH, { encoding: "utf8" }));
 
 function shouldAddPublisher(e) {
   return (
-    e.resource.resourceType === "CodeSystem" &&
-    e.resource.publisher === undefined &&
-    e.resource.meta.profile?.includes(
-      "http://hl7.org/fhir/StructureDefinition/shareablecodesystem",
-    )
+    ["ValueSet", "CodeSystem"].includes(e.resource.resourceType) &&
+    e.resource.publisher === undefined
   );
 }
 
-content.entry = content.entry.map((e) =>
-  shouldAddPublisher(e)
+function addPublisher(e) {
+  return shouldAddPublisher(e)
     ? { ...e, resource: { ...e.resource, publisher: "HL7 FHIR" } }
-    : e,
-);
+    : e;
+}
+
+function shouldAddDescription(e) {
+  return (
+    ["ValueSet", "CodeSystem"].includes(e.resource.resourceType) &&
+    e.resource.description === undefined
+  );
+}
+
+function addDescription(e) {
+  return shouldAddDescription(e)
+    ? {
+        ...e,
+        resource: { ...e.resource, description: "placeholder description" },
+      }
+    : e;
+}
+
+content.entry = content.entry.map(addPublisher).map(addDescription);
 
 fs.writeFileSync(PATH, JSON.stringify(content, null, 2));
