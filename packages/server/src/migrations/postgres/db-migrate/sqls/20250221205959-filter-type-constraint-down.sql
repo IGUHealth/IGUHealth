fbd023556d31de14c736ce86ea61d8b5fb79d698
confused-moniker <ralph@genfhi.app>
2025-02-21 15:10:07 -0600

    alter constraint on unique id

---
+++ packages/server/src/migrations/postgres/db-migrate/sqls/20250221205959-filter-type-constraint-down.sql
@@ -0,0 +1,9 @@
+ALTER TABLE r4_sp1_idx
+DROP CONSTRAINT r4_sp1_idx_tenant_r_id_key;
+
+ALTER TABLE r4_sp1_idx ADD CONSTRAINT r4_sp1_idx_tenant_r_id_key UNIQUE (tenant, r_id);
+
+ALTER TABLE r4b_sp1_idx
+DROP CONSTRAINT r4b_sp1_idx_tenant_r_id_key;
+
+ALTER TABLE r4b_sp1_idx ADD CONSTRAINT r4b_sp1_idx_tenant_r_id_key UNIQUE (tenant, r_id);
