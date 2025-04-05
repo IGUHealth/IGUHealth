f81cdae81344b216cd41cf9ce1204e068d4afdd8
confused-moniker <ralph@genfhi.app>
2025-04-04 19:18:54 -0500

    provider

--- packages/server/src/queue/providers/pg.ts
+++ packages/server/src/queue/providers/pg.ts
@@ -15,33 +15,120 @@ export type PGQueueConfig = {
   connection: db.Queryable;
 };
 
-export class PGQueue implements IQueue {
-  constructor(config: unknown) {}
+export class PGQueueBatch implements IQueue, IQueueBatch {
+  private readonly _connection: db.Queryable;
+  private readonly _transactionKey: string;
+  private _messages: Record<string, s.sub_queue.Insertable[]> = {};
 
-  async createTopic<T extends ITopic>(topic: T): Promise<boolean> {
-    // No need as can dynamically be created on insert.
-    return true;
+  constructor(config: PGQueueConfig) {
+    this._connection = config.connection;
+    this._transactionKey = "batch-" + Date.now();
   }
-  send<T extends ITopic>(
-    topic_id: T,
-    messages: ITopicMessage<T>[],
-  ): Promise<void> {
-    throw new Error("Method not implemented.");
+  async commit(): Promise<void> {
+    const messages = Object.values(this._messages).flat();
+    await db
+      .insert(
+        "sub_queue",
+        messages.map((m) => ({
+          ...m,
+          headers: m.headers as db.JSONValue,
+          value: m.value as db.JSONValue,
+        })),
+      )
+      .run(this._connection);
+  }
+  async abort(): Promise<void> {
+    this._messages = {};
   }
   sendTenant<
     Tenant extends TenantId,
     Type extends TopicType,
     T extends TenantTopic<Tenant, Type>,
   >(tenant: Tenant, topic: T, messages: ITopicMessage<T>[]): Promise<void> {
-    throw new Error("Method not implemented.");
+    return this.send(
+      topic,
+      messages.map((m) => ({ ...m, headers: { tenant, ...m.headers } })),
+    );
   }
-  batch(): Promise<IQueueBatch> {
-    throw new Error("Method not implemented.");
+  async batch(): Promise<IQueueBatch> {
+    return this;
   }
   isBatch(): boolean {
-    throw new Error("Method not implemented.");
+    return true;
   }
-  disconnect(): Promise<void> {
-    throw new Error("Method not implemented.");
+  async disconnect(): Promise<void> {
+    return;
   }
+  async createTopic<T extends ITopic>(_topic: T): Promise<boolean> {
+    // No need as can dynamically be created on insert.
+    return true;
+  }
+  async send<T extends ITopic>(
+    topic_id: T,
+    messages: ITopicMessage<T>[],
+  ): Promise<void> {
+    this._messages[topic_id] = [
+      ...(this._messages[topic_id] || []),
+      ...messages.map((m) => ({
+        ...m,
+        headers: m.headers as db.JSONValue,
+        value: m.value as db.JSONValue,
+        topic_id,
+      })),
+    ];
+  }
+}
+
+export class PGQueue implements IQueue {
+  private readonly _connection: db.Queryable;
+  constructor(config: PGQueueConfig) {
+    this._connection = config.connection;
+  }
+
+  async createTopic<T extends ITopic>(_topic: T): Promise<boolean> {
+    // No need as can dynamically be created on insert.
+    return true;
+  }
+  async send<T extends ITopic>(
+    topic_id: T,
+    messages: ITopicMessage<T>[],
+  ): Promise<void> {
+    await db
+      .insert(
+        "sub_queue",
+        messages.map((m) => ({
+          ...m,
+          headers: m.headers as db.JSONValue,
+          value: m.value as db.JSONValue,
+          topic_id,
+        })),
+      )
+      .run(this._connection);
+  }
+  async sendTenant<
+    Tenant extends TenantId,
+    Type extends TopicType,
+    T extends TenantTopic<Tenant, Type>,
+  >(tenant: Tenant, topic: T, messages: ITopicMessage<T>[]): Promise<void> {
+    await this.send(
+      topic,
+      messages.map((m) => ({
+        ...m,
+        headers: { tenant, ...m.headers },
+      })),
+    );
+  }
+  batch(): Promise<IQueueBatch> {
+    return db.transaction(
+      this._connection,
+      db.IsolationLevel.RepeatableRead,
+      async (txt) => {
+        return new PGQueueBatch({ connection: txt });
+      },
+    );
+  }
+  isBatch(): boolean {
+    return false;
+  }
+  async disconnect(): Promise<void> {}
 }
