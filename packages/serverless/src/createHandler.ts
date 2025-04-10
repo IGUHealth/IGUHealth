b53b480f8d3a61f18bf3fae820fbff10a8b760ba
confused-moniker <ralph@genfhi.app>
2025-04-10 14:06:01 -0500

    mod

--- packages/serverless/src/createHandler.ts
+++ packages/serverless/src/createHandler.ts
@@ -1,10 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Ajv from "ajv";
 import serverless from "serverless-http";
 
 import { createConsumerServices } from "@iguhealth/server/consumer/services";
 import { Message, MessageHandler } from "@iguhealth/server/consumer/types";
 
-function convertEvent(event: any): Message {
+import { QueueEvent } from "./schemas/worker-event.js";
+import workerEventSchema from "./schemas/worker-event.json" with { type: "json" };
+
+const ajv = new Ajv.default({});
+const eventValidator = ajv.compile(workerEventSchema);
+
+function convertEvent(event: QueueEvent): Message {
   return {
     headers: event.headers as Record<string, string>,
     value: event.value,
@@ -15,17 +23,26 @@ function convertEvent(event: any): Message {
   };
 }
 
+function validateEvent(event: any): event is QueueEvent {
+  const envValid = eventValidator(event);
+  if (!envValid) throw new Error(ajv.errorsText(eventValidator.errors));
+  return true;
+}
+
 export default async function createHandler(
   handler: MessageHandler<Awaited<ReturnType<typeof createConsumerServices>>>,
 ): Promise<serverless.Handler> {
   const services = await createConsumerServices();
 
   return async (event) => {
-    const message = convertEvent(event);
-    await handler(services, { topic: message.topic_id, messages: [message] });
+    if (validateEvent(event)) {
+      const message = convertEvent(event);
+      await handler(services, { topic: message.topic_id, messages: [message] });
 
-    return {
-      message: "ok",
-    };
+      return {
+        message: "ok",
+      };
+    }
+    throw new Error("Invalid event");
   };
 }
