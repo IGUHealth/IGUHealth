7340be657a1bb20a52a79883eba8f8ba3593e0ad
confused-moniker <ralph@genfhi.app>
2025-04-26 20:16:43 -0500

    qfix admin app

--- packages/admin-app/src/db/client.ts
+++ packages/admin-app/src/db/client.ts
@@ -6,15 +6,7 @@ import { createMiddlewareAsync } from "@iguhealth/client/middleware";
 import { AllInteractions, FHIRResponse } from "@iguhealth/client/types";
 import { FHIR_VERSION } from "@iguhealth/fhir-types/versions";
 
-type CachedClient = AsynchronousClient<
-  {
-    client: AsynchronousClient<
-      { client: ReturnType<typeof createAdminAppClient> },
-      unknown
-    >;
-  },
-  unknown
->;
+type CachedClient = AsynchronousClient<HTTPContext>;
 
 export const getClient = atom<ReturnType<typeof createAdminAppClient>>(
   // Q Hack to avoid uneccessary checks.
@@ -33,49 +25,57 @@ export function createAdminAppClient(
   client: ReturnType<typeof createHTTPClient>,
 ): CachedClient {
   return new AsynchronousClient(
-    { client: client },
     createMiddlewareAsync<
       { client: ReturnType<typeof createHTTPClient> },
       HTTPContext
-    >([
-      async (context) => {
+    >({ client: client }, [
+      async (state, context) => {
         switch (context.request.type) {
           case "invoke-request": {
             switch (context.request.operation) {
               case "expand": {
                 const requestString = JSON.stringify(context.request);
                 if (!cachedResponse[requestString]) {
-                  cachedResponse[requestString] = context.state.client.request(
+                  cachedResponse[requestString] = state.client.request(
                     context.ctx,
                     context.request,
                   );
                 }
 
-                return {
-                  ...context,
-                  response: await cachedResponse[requestString],
-                };
+                return [
+                  state,
+                  {
+                    ...context,
+                    response: await cachedResponse[requestString],
+                  },
+                ];
               }
 
               default:
-                return {
-                  ...context,
-                  response: await context.state.client.request(
-                    context.ctx,
-                    context.request,
-                  ),
-                };
+                return [
+                  state,
+                  {
+                    ...context,
+                    response: await state.client.request(
+                      context.ctx,
+                      context.request,
+                    ),
+                  },
+                ];
             }
           }
 
           default:
-            return {
-              ...context,
-              response: await context.state.client.request(
-                context.ctx,
-                context.request,
-              ),
-            };
+            return [
+              state,
+              {
+                ...context,
+                response: await state.client.request(
+                  context.ctx,
+                  context.request,
+                ),
+              },
+            ];
         }
       },
     ]),
