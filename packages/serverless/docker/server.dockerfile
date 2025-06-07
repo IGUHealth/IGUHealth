be06e551234c9ffe17159690adf1238ba392a236
confused-moniker <ralph@genfhi.app>
2025-06-07 00:14:36 -0500

    simplify serverless layer

--- packages/serverless/docker/server.dockerfile
+++ packages/serverless/docker/server.dockerfile
@@ -1,14 +1,5 @@
-FROM alpine:3.21.3 AS layer-copy 
-
-RUN mkdir -p /opt
-
-COPY ./layers/lambda-parameter-extension:17.zip layer.zip
-RUN unzip layer.zip -d /opt
-RUN rm layer.zip
-
 FROM public.ecr.aws/lambda/nodejs:22
 
-COPY --from=layer-copy /opt /opt
 COPY docker/package.json package.json
 RUN npm install --ignore-scripts
 
--- packages/serverless/src/lambdas/server.ts
+++ packages/serverless/src/lambdas/server.ts
@@ -5,6 +5,11 @@ import createServer from "@iguhealth/server";
 const server = await createServer();
 const _handler = serverless(server);
 
-export const handler: serverless.Handler = (event: object, context: object) => {
-  return _handler(event, context);
+export const handler: serverless.Handler = async (
+  event: object,
+  context: object,
+) => {
+  const response = await _handler(event, context);
+  console.log("Response:", response);
+  return response;
 };
